1c5aced48ef2018126b5d7013033c2285ccb59d9
Tomislav Markovski <tomislav@streetcred.id>
2021-02-25 21:04:54 +0000

    Merged PR 1965: .NET sdk project. Unit test runner for server

---
+++ dotnet/Trinsic.Sdk.Tests/WalletTests.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Threading.Tasks;
+using Google.Protobuf;
+using Grpc.Net.Client;
+using Trinsic.Services;
+using Xunit;
+
+namespace Trinsic.Sdk.Tests
+{
+    public class WalletTests
+    {
+        private readonly WalletService service;
+
+        public WalletTests()
+        {
+            service = new WalletService();
+        }
+
+        [Fact]
+        public async Task Test1()
+        {
+            var profile = await service.CreateWallet();
+
+            System.IO.File.WriteAllBytes("~/.trinsic/profile.bin", profile.ToByteArray());
+
+            Assert.NotNull(profile);
+        }
+
+        [Fact]
+        public async Task CanGetProviderConfiguration()
+        {
+            var channel = GrpcChannel.ForAddress("http://localhost:5000/");
+            var client = new Wallet.WalletClient(channel);
+
+            var configuration = await client.GetProviderConfigurationAsync(new GetProviderConfigurationRequest());
+
+            Assert.NotNull(configuration);
+            Assert.NotNull(configuration.DidDocument);
+            Assert.NotNull(configuration.KeyAgreementKeyId);
+        }
+    }
+}
---
+++ dotnet/Trinsic.Sdk/WalletService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using Trinsic.Services;
+using DIDComm.Messaging;
+using DComm = DIDComm.Messaging.DIDComm;
+using Google.Protobuf;
+
+namespace Trinsic.Sdk
+{
+    public class WalletService
+    {
+        public WalletService(string serviceAddress = "http://localhost:5000")
+        {
+            Channel = GrpcChannel.ForAddress(serviceAddress);
+            Client = new Wallet.WalletClient(Channel);
+        }
+
+        public GrpcChannel Channel { get; }
+        public Wallet.WalletClient Client { get; }
+
+        public async Task<WalletProfile> CreateWallet()
+        {
+            // Generate new DID used by the current device
+            var key = DIDKey.Generate(new GenerateKeyRequest
+            {
+                KeyType = KeyType.Ed25519
+            });
+
+            // Sign something to prove ownership
+            var signed = DComm.Sign(new SignRequest
+            {
+                Key = key.Key.First(),
+                Payload = key.DidDocument.ToByteString()
+            });
+
+            // Invoke create wallet
+            var response = await Client.CreateWalletAsync(new CreateWalletRequest
+            {
+                Description = "My Cloud Wallet"
+            });
+
+            // This profile should be stored and supplied later
+            return new WalletProfile
+            {
+                WalletId = response.WalletId,
+                DidDocument = key.DidDocument,
+                Invoker = key.Key.First().Kid
+            };
+        }
+    }
+}
