975151438178c5647ccfde823006635287d16069
Tomislav Markovski <tomislav@trinsic.id>
2021-05-17 20:35:40 -0400

    Add copyfiles support

--- node/test/WalletService.test.js
+++ node/test/WalletService.test.js
@@ -1,9 +1,11 @@
 var test = require("ava");
 var TrinsicWalletService = require("../dist/WalletService.js").TrinsicWalletService;
 
-test("create", async t => {
+test("get provider configuration", async t => {
     var service = new TrinsicWalletService();
     var configuration = await service.getProviderConfiguration();
 
     t.pass();
 });
+
+test("debug", t => t.pass());
