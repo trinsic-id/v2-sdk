c1eb17dd25fdb096671ac695d0923be56446a6aa
Tomislav Markovski <tomislav@trinsic.id>
2021-06-14 12:50:09 -0400

    Add provider configur print

--- dotnet/Samples/VaccinationSample/Program.cs
+++ dotnet/Samples/VaccinationSample/Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.IO;
+using Google.Protobuf.WellKnownTypes;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Trinsic;
+using Trinsic.Services;
 
 Console.WriteLine("Hello World!");
 
 var walletService = new WalletService("http://20.84.172.172:5000");
+
+var configuration = await walletService.Client.GetProviderConfigurationAsync(new GetProviderConfigurationRequest());
+
+Console.WriteLine("Provider Configuration:");
+Console.WriteLine(configuration.DidDocument.ToJObject().ToString(Formatting.Indented));
 
 var alice = await walletService.CreateWallet();
 var clinic = await walletService.CreateWallet();
