8ec46e5a411ab226145ec9886dc2b245f8a55fb3
Tomislav Markovski <tomislav@trinsic.id>
2021-06-14 10:41:33 -0400

    Sample dotnet project

---
+++ dotnet/Samples/VaccinationSample/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Trinsic;
+
+Console.WriteLine("Hello World!");
+
+var walletService = new WalletService("http://localhost:5000");
+
+var alice = await walletService.CreateWallet();
+var clinic = await walletService.CreateWallet();
+var venue = await walletService.CreateWallet();
+
+
+walletService.SetProfile(alice);
+
+var credentialJson = File.ReadAllText("./vaccination-certificate-unsigned.jsonld");
+var credential = await walletService.IssueCredential(document: JObject.Parse(credentialJson));
+
+Console.WriteLine(credential.ToString(Formatting.Indented));
