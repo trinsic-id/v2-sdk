6712efc97586751111a23949582aa64510ec39f0
Tomislav Markovski <tomislav@trinsic.id>
2021-06-15 11:26:24 -0400

    Vaccination demo

--- docs/walkthrough/vaccination-net.md
+++ docs/walkthrough/vaccination-net.md
@@ -4,6 +4,24 @@
 walkthrough/snippets/intro-infrastructure.md
 --8<----
 
+## Configure services
+
+```csharp
+var walletService = new WalletService("<ECOSYSTEM SERVICE URL>");
+```
+
+## Setup wallet profiles
+
+Let's create three different profiles, each connected to a separate wallet. Since we are using a single console app for this demo, we will simply set the active profile before each interaction to designate which actor is currently taking action.
+To create a new wallet profile, we use the [Create Wallet](/reference/services/wallet-service/#create-wallet) feature.
+
+```csharp
+// SETUP ACTORS
+// Create 3 different profiles for each participant in the scenario
+var alice = await walletService.CreateWallet();
+var clinic = await walletService.CreateWallet();
+var venue = await walletService.CreateWallet();
+```
 
 
 ??? example "Run example in Replit"
--- dotnet/Samples/VaccinationSample/Program.cs
+++ dotnet/Samples/VaccinationSample/Program.cs
@@ -1,27 +1,50 @@
 ﻿using System;
 using System.IO;
-using Google.Protobuf.WellKnownTypes;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Trinsic;
 using Trinsic.Services;
 
-Console.WriteLine("Hello World!");
-
 var walletService = new WalletService("http://20.84.172.172:5000");
 
-var configuration = await walletService.Client.GetProviderConfigurationAsync(new GetProviderConfigurationRequest());
-
-Console.WriteLine("Provider Configuration:");
-Console.WriteLine(configuration.DidDocument.ToJObject().ToString(Formatting.Indented));
-
+// SETUP ACTORS
+// Create 3 different profiles for each participant in the scenario
 var alice = await walletService.CreateWallet();
 var clinic = await walletService.CreateWallet();
 var venue = await walletService.CreateWallet();
 
-walletService.SetProfile(alice);
+// ISSUE CREDENTIAL
+// Sign a credential as the clinic and send it to Alice
+walletService.SetProfile(clinic);
 
 var credentialJson = File.ReadAllText("./vaccination-certificate-unsigned.jsonld");
 var credential = await walletService.IssueCredential(document: JObject.Parse(credentialJson));
 
+Console.WriteLine("Credential:");
 Console.WriteLine(credential.ToString(Formatting.Indented));
+
+// STORE CREDENTIAL
+// Alice stores the credential in her cloud wallet.
+walletService.SetProfile(alice);
+
+var itemId = await walletService.InsertItem(credential);
+
+// SHARE CREDENTIAL
+// Alice shares the credential with the venue.
+// The venue has communicated with alice the details of the credential
+// that they require expressed as a JSON-LD frame.
+walletService.SetProfile(alice);
+
+var proofRequestJson = File.ReadAllText("./vaccination-certificate-frame.jsonld");
+var credentialProof = await walletService.CreateProof(itemId, JObject.Parse(proofRequestJson));
+
+Console.WriteLine("Proof:");
+Console.WriteLine(credentialProof.ToString(Formatting.Indented));
+
+
+// VERIFY CREDENTIAL
+walletService.SetProfile(venue);
+
+var valid = await walletService.VerifyProof(credentialProof);
+
+Console.WriteLine($"Verification result: {valid}");
