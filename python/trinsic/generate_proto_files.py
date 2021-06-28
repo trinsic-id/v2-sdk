117fef7dff47e2cc586d2965db6e6b7bf963ca12
fundthmcalculus <polygonguru@gmail.com>
2021-06-28 14:42:22 -0400

    Update to python-betterproto

--- python/trinsic/generate_proto_files.py
+++ python/trinsic/generate_proto_files.py
@@ -3,22 +3,7 @@ import grpc_tools.protoc as protoc
 from os.path import join, dirname, abspath
 import glob
 
-
-def generate_proto_files(base_path: str = None, file_path: str = None) -> None:
-    """
-    Generate the protobuf interface files.
-    :return:
-    """
-    file_path = file_path or abspath(dirname(abspath(__file__)))
-    base_path = base_path or 'C:\\work\\okapi-proto'
-    proto_file_path = abspath(join(base_path, "**", "*.proto"))
-    base_command = ['C:\\bin\\protoc.exe', f'--proto_path={base_path}', f'--proto_path=C:\\bin',
-                    f'--python_out={file_path}']
-    # Get all the included proto files
-    base_command.extend(glob.glob(proto_file_path, recursive=True))
-    result = subprocess.run(base_command)
-    result.check_returncode()
-    print('Complete!')
+import pkg_resources
 
 
 def generate_better_proto_files() -> None:
@@ -26,22 +11,19 @@ def generate_better_proto_files() -> None:
     Generate the protobuf interface files using the python library https://github.com/danielgtaylor/python-betterproto
     :return:
     """
-    output_path = abspath(dirname(abspath(__file__)))
-    base_path = abspath(join(output_path, '', '../../../..', 'proto'))
-    proto_file_path = abspath(join(base_path, "**", "*.proto"))
+    file_path = abspath(dirname(abspath(__file__)))
+    base_path = abspath(join(file_path, '..', '..', 'proto'))
+    proto_file_path = abspath(join(base_path, "*.proto"))
+    output_path = abspath(join(file_path, 'proto'))
+    # Come up with better locations, import google defaults from the package location (see code in protoc.main)
+    proto_include = pkg_resources.resource_filename('grpc_tools', '_proto').replace("lib", "Lib")
     # Inject an empty python code file path to mimic the first argument.
-    # TODO - Come up with better locations, import google defaults from the package location (see code in protoc.main)
-    base_command = ['', f'--proto_path=C:\\bin', f'--proto_path={base_path}', f'--python_betterproto_out={output_path}']
+    base_command = ['', '-I', base_path, f'--python_betterproto_out=proto']
     # Get all the included proto files
     base_command.extend(glob.glob(proto_file_path, recursive=True))
-    print(base_command)
+    base_command.append(f'-I{proto_include}')
     protoc.main(base_command)
 
 
-def download_binary_files() -> None:
-    # TODO - Download the appropriate library files
-    raise NotImplementedError
-
-
 if __name__ == "__main__":
-    generate_proto_files()
+    generate_better_proto_files()
