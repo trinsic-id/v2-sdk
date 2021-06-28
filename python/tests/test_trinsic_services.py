75eea96bf993627ea17426e0d970cde8bc6d6d5e
fundthmcalculus <polygonguru@gmail.com>
2021-06-28 17:16:49 -0400

    no message

--- python/tests/test_trinsic_services.py
+++ python/tests/test_trinsic_services.py
@@ -1,9 +1,62 @@
+import json
 import unittest
 
+from trinsic.services import WalletService
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+
+class MyTestCase(unittest.IsolatedAsyncioTestCase):
+    async def test_trinsic_service_demo(self):
+        wallet_service = WalletService("http://tomislav-staging.eastus.azurecontainer.io:5000")
+
+        # SETUP ACTORS
+        # Create 3 different profiles for each participant in the scenario
+        allison = await wallet_service.create_wallet()
+        clinic = await wallet_service.create_wallet()
+        airline = await wallet_service.create_wallet()
+
+        # Store profile for later use
+        # File.WriteAllBytes("allison.bin", allison.ToByteString().ToByteArray());
+
+        # Create profile from existing data
+        # var allison = WalletProfile.Parser.ParseFrom(File.ReadAllBytes("allison.bin"));
+
+        # ISSUE CREDENTIAL
+        # Sign a credential as the clinic and send it to Allison
+        wallet_service.set_profile(clinic)
+        credential_json = ""
+        with open("vaccination-certificate-unsigned.jsonld", "r") as fid:
+            credential_json = json.load(fid)
+
+        credential = await wallet_service.issue_credential(credential_json)
+
+        print(f"Credential: {credential}")
+
+        # STORE CREDENTIAL
+        # Alice stores the credential in her cloud wallet.
+        wallet_service.set_profile(allison)
+        item_id = await wallet_service.insert_item(credential)
+
+        # SHARE CREDENTIAL
+        # Allison shares the credential with the venue.
+        # The venue has communicated with Allison the details of the credential
+        # that they require expressed as a JSON-LD frame.
+        wallet_service.set_profile(allison)
+
+        proof_request_json = ""
+        with open("vaccination-certificate-frame.jsonld", "r") as fid2:
+            proof_request_json = json.load(fid2)
+
+        credential_proof = await wallet_service.create_proof(document_id=item_id, reveal_document=proof_request_json)
+
+        print(f"Proof: {credential_proof}")
+
+        # VERIFY CREDENTIAL
+        # The airline verifies the credential
+        wallet_service.set_profile(airline)
+        valid = await wallet_service.verify_proof(credential_proof)
+
+        print(f"Verification result: {valid}")
+        self.assertTrue(valid)
 
 
 if __name__ == '__main__':
---
+++ python/trinsic/services.py
@@ -0,0 +1,146 @@
+import base64
+import datetime
+import urllib.parse
+
+import betterproto
+from grpclib.client import Channel
+from grpc.aio import Metadata
+from okapi.keys import LDProofs, DIDKey, DIDComm
+from okapi.okapi_utils import dictionary_to_struct
+from okapi.proto.okapi.keys import JsonWebKey, ResolveRequest, GenerateKeyRequest, KeyType
+from okapi.proto.okapi.proofs import CreateProofRequest, LdSuite
+from okapi.proto.okapi.transport import PackRequest, UnpackRequest
+
+from trinsic.proto.trinsic.services import WalletProfile, WalletStub, CredentialStub, CreateWalletRequest, \
+    CreateWalletResponse, SearchResponse, ProviderStub, InviteRequest, InviteResponse, InvitationStatusRequest, \
+    InvitationStatusResponse
+
+
+class ServiceBase:
+    def __init__(self):
+        self.cap_invocation: str = ""
+
+    @property
+    def metadata(self) -> Metadata:
+        if not self.cap_invocation:
+            raise Exception("Profile not set")
+        return Metadata(("Capability-Invocation", self.cap_invocation))
+
+    def set_profile(self, profile: WalletProfile):
+        capability_doc = {"@context": "https://w3id.org/security/v2",
+                          "invocationTarget": profile.wallet_id,
+                          "proof": {
+                              "proofPurpose": "capabilityInvocation",
+                              "created": datetime.datetime.now(),
+                              "capability": profile.capability
+                          }}
+
+        proof_response = LDProofs.create(CreateProofRequest(key=JsonWebKey().parse(profile.invoker_jwk),
+                                                            document=dictionary_to_struct(capability_doc),
+                                                            suite=LdSuite.JcsEd25519Signature2020))
+
+        self.cap_invocation = base64.encodestring(bytes(proof_response.signed_document.to_json(), encoding="utf-8"))
+
+
+class WalletService(ServiceBase):
+    def __init__(self, service_address: str = "http://localhost:5000", channel: Channel = None):
+        super().__init__()
+        if not channel:
+            service_url = urllib.parse.urlsplit(service_address)
+            channel = Channel(host=f"{service_url.hostname}", port=service_url.port)
+
+        self.channel = channel
+        self.client = WalletStub(self.channel)
+        self.credential_client = CredentialStub(self.channel)
+
+    async def register_or_connect(self, email: str):
+        await self.client.connect_external_identity(email=email)
+
+    async def create_wallet(self, security_code: str = None) -> WalletProfile:
+        configuration = await self.client.get_provider_configuration()
+        resolve_response = DIDKey.resolve(ResolveRequest(did=configuration.key_agreement_key_id))
+        provider_exchange_key = [x for x in resolve_response.keys if x.kid == configuration.key_agreement_key_id][0]
+
+        my_key = DIDKey.generate(GenerateKeyRequest(key_type=KeyType.Ed25519))
+        my_exchange_key = [x for x in my_key.key if x.crv == "X25519"][0]
+        my_did_document = my_key.did_document.to_dict()
+
+        packed_message = DIDComm.pack(PackRequest(sender_key=my_exchange_key, receiver_key=provider_exchange_key,
+                                                  plaintext=bytes(CreateWalletRequest(description="My Cloud Wallet",
+                                                                                      controller=str(
+                                                                                          my_did_document['id']),
+                                                                                      security_code=security_code or ""))))
+
+        response = await self.client.create_wallet_encrypted(iv=packed_message.message.iv,
+                                                             recipients=packed_message.message.recipients,
+                                                             ciphertext=packed_message.message.ciphertext,
+                                                             aad=packed_message.message.aad,
+                                                             tag=packed_message.message.tag)
+
+        decrypted_response = DIDComm.unpack(UnpackRequest(message=response))
+        create_wallet_response = CreateWalletResponse().parse(decrypted_response.plaintext)
+
+        return WalletProfile(wallet_id=create_wallet_response.wallet_id,
+                             capability=create_wallet_response.capability,
+                             did_document=my_key.did_document,
+                             invoker=create_wallet_response.invoker,
+                             invoker_jwk=bytes(my_key.key[0]))
+
+    async def issue_credential(self, document: dict) -> dict:
+        try:
+            response = await self.credential_client.issue(document=dictionary_to_struct(document))
+            return response.document.to_dict()
+        except Exception as e:
+            print(e)
+            raise e
+
+    async def search(self, query: str = "SELECT * from c") -> SearchResponse:
+        return await self.client.search(query=query)
+
+    async def insert_item(self, item: dict) -> str:
+        return (await self.client.insert_item(item=dictionary_to_struct(item))).item_id
+
+    async def send(self, document: dict, email: str):
+        await self.credential_client.send(email=email, document=dictionary_to_struct(document))
+
+    async def create_proof(self, document_id: str, reveal_document: dict) -> dict:
+        return (await self.credential_client.create_proof(document_id=document_id, reveal_document=dictionary_to_struct(
+            reveal_document))).proof_document.to_dict()
+
+    async def verify_proof(self, proof_document: dict) -> bool:
+        return (await self.credential_client.verify_proof(proof_document=dictionary_to_struct(proof_document))).valid
+
+
+class ProviderService(ServiceBase):
+    def __init__(self, service_address: str = "http://localhost:5000", channel: Channel = None):
+        super().__init__()
+        if not channel:
+            service_url = urllib.parse.urlsplit(service_address)
+            channel = Channel(host=f"{service_url.hostname}", port=service_url.port)
+
+        self.channel = channel
+        self.provider_client = ProviderStub(self.channel)
+
+    async def invite_participant(self, request: InviteRequest) -> InviteResponse:
+        field_name, value = betterproto.which_one_of(request, "contact_method")
+        if not field_name:
+            raise Exception("Contact method must be set")
+
+        try:
+            return await self.provider_client.invite(participant=request.participant, description=request.description,
+                                                     didcomm_invitation=request.didcomm_invitation, phone=request.phone,
+                                                     email=request.email)
+        except Exception as e:
+            print(e)
+            raise e
+
+    async def invitation_status(self, request: InvitationStatusRequest) -> InvitationStatusResponse:
+        if not request.invitation_id or not request.invitation_id.strip():
+            raise Exception("Onboarding reference ID must be set.")
+
+        try:
+            return await self.provider_client.invitation_status(invitation_id=request.invitation_id)
+        except Exception as e:
+            print(e)
+            raise e
+
