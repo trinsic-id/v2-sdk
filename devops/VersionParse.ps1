6a7e1a3762ea4f1322ee5f60fd78c9d85d45e728
Scott Phillips <polygonguru@gmail.com>
2021-07-02 12:29:40 -0400

    Build script support

---
+++ devops/VersionParse.ps1
@@ -0,0 +1,13 @@
+﻿function Get-PythonVersion {
+    param ([string]$tag)
+	
+    $match = $tag -match "([v]?(?<MAJOR>\d*)\.(?<MINOR>\d*)\.?(?<PATCH>\d*))(-\w*\.(?<BUILD>\d*))?"
+    if ($match) {
+        $pythonVersion = $Matches.MAJOR + "." + $Matches.MINOR
+        $pythonVersion += if (![string]::IsNullOrWhiteSpace($Matches.PATCH)) { "." + $Matches.PATCH }
+        $pythonVersion += if (![string]::IsNullOrWhiteSpace($Matches.BUILD)) { "b" + $Matches.BUILD }
+        return $pythonVersion
+    } else {
+        throw "No version match"
+    }
+}
