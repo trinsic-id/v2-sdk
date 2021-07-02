a77553ef1e388d3f528b9e1d7a8e30a125d30663
Scott Phillips <polygonguru@gmail.com>
2021-07-02 13:25:01 -0400

    tabs to spaces like god intended.

--- devops/BuildPython.ps1
+++ devops/BuildPython.ps1
@@ -1,54 +1,54 @@
 param
 (
     [string]$GitTag,
-	[switch]$UseDevelopOkapi = $false
+    [switch]$UseDevelopOkapi = $false
 )
 
 . "$PSScriptRoot/VersionParse.ps1"
 
 function Activate-Venv {
-	try {
-		if ($IsWindows) {
-			 .\venv\Scripts\activate
-		} else {
-			./venv/bin/activate
-		}
-	} catch {
-	} finally {
-		if ($IsWindows) {
-			Write-Output (where python)
-		} else {
-			Write-Output (which python)
-		}
-	}
+    try {
+        if ($IsWindows) {
+             .\venv\Scripts\activate
+        } else {
+            ./venv/bin/activate
+        }
+    } catch {
+    } finally {
+        if ($IsWindows) {
+            Write-Output (where python)
+        } else {
+            Write-Output (which python)
+        }
+    }
 }
 function Install-Requirements {
-	python -m pip install --upgrade pip
-	python -m pip install -r requirements.txt
-	python -m pip install pytest pytest-cov build
-	if ($UseDevelopOkapi) {
-		python -m pip install --pre trinsic-okapi
-	}
-	python ./trinsic/generate_proto_files.py
+    python -m pip install --upgrade pip
+    python -m pip install -r requirements.txt
+    python -m pip install pytest pytest-cov build
+    if ($UseDevelopOkapi) {
+        python -m pip install --pre trinsic-okapi
+    }
+    python ./trinsic/generate_proto_files.py
 }
 function Run-Tests {
-	python -m pytest --cache-clear ./tests --junitxml=pytest_junit.xml --cov=.
+    python -m pytest --cache-clear ./tests --junitxml=pytest_junit.xml --cov=.
 }
 function Build-Package {
-	try {
-		# Get release version from tag and set the setup.cfg file
-		$pythonPackageVersion = Get-PythonVersion($GitTag)
-		$setupConfig = Get-Content ./setup.cfg
-		for ($ij = 0; $ij -lt $setupConfig.Length; $ij++) {
-			if ($setupConfig[$ij].StartsWith("version")) {
-				$setupConfig[$ij] = "version = " + $pythonPackageVersion
-			}
-		}
-		Set-Content -path ./setup.cfg -value $setupConfig
-	} catch {
-	}finally {
-		python -m build --sdist --wheel --outdir dist/ .
-	}
+    try {
+        # Get release version from tag and set the setup.cfg file
+        $pythonPackageVersion = Get-PythonVersion($GitTag)
+        $setupConfig = Get-Content ./setup.cfg
+        for ($ij = 0; $ij -lt $setupConfig.Length; $ij++) {
+            if ($setupConfig[$ij].StartsWith("version")) {
+                $setupConfig[$ij] = "version = " + $pythonPackageVersion
+            }
+        }
+        Set-Content -path ./setup.cfg -value $setupConfig
+    } catch {
+    }finally {
+        python -m build --sdist --wheel --outdir dist/ .
+    }
 }
 
 # Setup
@@ -61,7 +61,7 @@ Run-Tests
 Build-Package
 # Leave the venv
 try {
-	deactivate
+    deactivate
 } catch {
 }
 Set-Location $InvocationPath
