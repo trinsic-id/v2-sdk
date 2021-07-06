49bd6de22cbfdb0cb349a833ddcabe612b0dd46e
MichaelEdwardBlack <michaelblack117@gmail.com>
2021-07-06 14:29:13 -0600

    fixed bash syntax

--- web/setenv.sh
+++ web/setenv.sh
@@ -2,3 +2,4 @@ if [[ -n $TRINSIC_TEST_URL ]]; then
     echo "env = { ENDPOINT: 'http://$TRINSIC_TEST_URL' }" > ./test/env.js
 else
     echo "env = { ENPOINT: 'http://localhost:5000' }" > ./test/env.js
+fi
