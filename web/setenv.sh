9845e9220eba9e8d0c7866638f37346b1b034017
MichaelEdwardBlack <michaelblack117@gmail.com>
2021-07-06 14:56:06 -0600

    changed from bash to powershell

--- web/setenv.sh
+++
@@ -1,5 +0,0 @@
-if [[ -n $TRINSIC_TEST_URL ]]; then
-    echo "exports.env = { ENDPOINT: 'http://$TRINSIC_TEST_URL' }" > ./test/env.js
-else
-    echo "exports.env = { ENPOINT: 'http://localhost:5000' }" > ./test/env.js
-fi
