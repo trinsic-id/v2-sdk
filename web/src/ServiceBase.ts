34cd6948d7ea97dde1cedc5492ab6ac45a993943
Tomislav Markovski <tomislav@trinsic.id>
2021-07-30 09:08:33 -0400

    Update web

--- web/src/ServiceBase.ts
+++ web/src/ServiceBase.ts
@@ -1,9 +1,8 @@
-//const okapi = require("@trinsic/okapi");
-import okapi from '@trinsic/okapi';
+import { CreateProofRequest, JsonWebKey, LdProofs, LdSuite } from "@trinsic/okapi";
 import { Metadata } from "grpc-web";
-import { WalletProfile } from './proto/WalletService_pb';
-import { Struct } from 'google-protobuf/google/protobuf/struct_pb';
-const Buffer = require('buffer').Buffer;
+import { WalletProfile } from "./proto/WalletService_pb";
+import { Struct } from "google-protobuf/google/protobuf/struct_pb";
+import { Buffer } from "buffer";
 
 export default abstract class ServiceBase {
   capInvocation: string;
@@ -17,23 +16,25 @@ export default abstract class ServiceBase {
   async setProfile(profile: WalletProfile): Promise<void> {
     let capabilityDocument = {
       "@context": "https://wid.org/security/v2",
-      "invocationTarget": profile.getWalletId(),
-      "proof": {
-        "proofPurpose": "capabilityInvocation",
-        "created": new Date().toISOString(),
-        "capability": profile.getCapability()
-      }
+      invocationTarget: profile.getWalletId(),
+      proof: {
+        proofPurpose: "capabilityInvocation",
+        created: new Date().toISOString(),
+        capability: profile.getCapability(),
+      },
     };
 
-    let proofRequest = new okapi.CreateProofRequest();
-    proofRequest.setDocument(Struct.fromJavaScript(capabilityDocument));
-    proofRequest.setKey(okapi.JsonWebKey.deserializeBinary(profile.getInvokerJwk_asU8()));
-    proofRequest.setSuite(okapi.LdSuite.JCSED25519SIGNATURE2020);
+    let proofRequest = new CreateProofRequest()
+      .setDocument(Struct.fromJavaScript(capabilityDocument))
+      .setKey(JsonWebKey.deserializeBinary(profile.getInvokerJwk_asU8()))
+      .setSuite(LdSuite.JCSED25519SIGNATURE2020);
 
-    let proofResponse = await okapi.LdProofs.generate(proofRequest)
+    let proofResponse = await LdProofs.generate(proofRequest);
 
     // Set the auth field to the signed document by converting it back
     // to JSON and encoding it in base64
-    this.capInvocation = Buffer.from(JSON.stringify(proofResponse.getSignedDocument().toJavaScript())).toString('base64');  
+    this.capInvocation = Buffer.from(JSON.stringify(proofResponse.getSignedDocument().toJavaScript())).toString(
+      "base64"
+    );
   }
 }
