ed57daa9b3dc88b76300bee5b212e893ade39521
Scott Phillips <polygonguru@gmail.com>
2021-09-09 16:10:30 -0400

    build consistently (#113)

--- devops/BuildPython.ps1
+++ devops/BuildPython.ps1
@@ -2,56 +2,78 @@ param
 (
     [AllowNull()][string]$GitTag = '',
     [AllowNull()][string]$PackageVersion = '',
+    [AllowNull()][Boolean]$RequirementsOnly = $false,
     [switch]$UseDevelopOkapi = $false
 )
 
 . "$PSScriptRoot/VersionParse.ps1"
 
-function Activate-Venv {
-    try {
-        if ($IsWindows) {
-             .\venv\Scripts\activate
-        } else {
+function Activate-Venv
+{
+    try
+    {
+        if ($IsWindows)
+        {
+            .\venv\Scripts\activate
+        }
+        else
+        {
             ./venv/bin/activate
         }
-    } catch {
-    } finally {
+    }
+    catch
+    {
+    }
+    finally
+    {
         Write-Output (Where-Object python)
     }
 }
-function Install-Requirements {
+function Install-Requirements
+{
     python -m pip install --upgrade pip
     python -m pip install -r requirements.txt
     python -m pip install pytest pytest-cov build
-    if ($UseDevelopOkapi) {
+    if ($UseDevelopOkapi)
+    {
         python -m pip install --pre trinsic-okapi
     }
     python ./trinsic/generate_proto_files.py
 }
-function Run-Tests {
-    python -m pytest --cache-clear ./tests --junitxml=pytest_junit.xml --cov=.
+function Run-Tests
+{
+    python -m pytest --cache-clear ./tests --junitxml = pytest_junit.xml --cov = .
 }
-function Set-Python-Version {
+function Set-Python-Version
+{
     param ([string]$pythonPackageVersion = '')
     $setupConfig = Get-Content ./setup.cfg
     for ($ij = 0; $ij -lt $setupConfig.Length; $ij++) {
-        if ($setupConfig[$ij].StartsWith("version")) {
+        if ( $setupConfig[$ij].StartsWith("version"))
+        {
             $setupConfig[$ij] = "version = " + $pythonPackageVersion
         }
     }
     Set-Content -path ./setup.cfg -value $setupConfig
 }
-function Build-Package {
-    try {
+function Build-Package
+{
+    try
+    {
         # Get release version from tag and set the setup.cfg file
         $pythonPackageVersion = Get-PythonVersion($GitTag)
         Set-Python-Version($pythonPackageVersion)
-    } catch {
-        if (-not [string]::IsNullOrWhitespace($PackageVersion)) {
+    }
+    catch
+    {
+        if (-not [string]::IsNullOrWhitespace($PackageVersion))
+        {
             Set-Python-Version($PackageVersion)
         }
-    }finally {
-        
+    }
+    finally
+    {
+
         python -m build --sdist --wheel --outdir dist/ .
     }
 }
@@ -62,11 +84,17 @@ cd "$PSScriptRoot/../python"
 # Do the things
 Activate-Venv
 Install-Requirements
-Run-Tests
-Build-Package
+if (!$RequirementsOnly)
+{
+    Run-Tests
+    Build-Package
+}
 # Leave the venv
-try {
+try
+{
     deactivate
-} catch {
+}
+catch
+{
 }
 Set-Location $InvocationPath
