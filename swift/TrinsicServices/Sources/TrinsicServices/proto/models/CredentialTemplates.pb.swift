8a4e99f6412b0c7cc88a4bd1443a9eb7df955d79
Scott Phillips <polygonguru@gmail.com>
2021-09-10 16:41:31 -0400

    swift updates

--- swift/TrinsicServices/Sources/TrinsicServices/proto/models/CredentialTemplates.pb.swift
+++ swift/TrinsicServices/Sources/TrinsicServices/proto/models/CredentialTemplates.pb.swift
@@ -20,68 +20,68 @@ fileprivate struct _GeneratedWithProtocGenSwiftVersion: SwiftProtobuf.ProtobufAP
   typealias Version = _2
 }
 
-struct Trinsic_Services_CreateCredentialTemplateRequest {
+public struct Trinsic_Services_CreateCredentialTemplateRequest {
   // SwiftProtobuf.Message conformance is added in an extension below. See the
   // `Message` and `Message+*Additions` files in the SwiftProtobuf library for
   // methods supported on all messages.
 
-  var unknownFields = SwiftProtobuf.UnknownStorage()
+  public var unknownFields = SwiftProtobuf.UnknownStorage()
 
-  init() {}
+  public init() {}
 }
 
-struct Trinsic_Services_CreateCredentialTemplateResponse {
+public struct Trinsic_Services_CreateCredentialTemplateResponse {
   // SwiftProtobuf.Message conformance is added in an extension below. See the
   // `Message` and `Message+*Additions` files in the SwiftProtobuf library for
   // methods supported on all messages.
 
-  var unknownFields = SwiftProtobuf.UnknownStorage()
+  public var unknownFields = SwiftProtobuf.UnknownStorage()
 
-  init() {}
+  public init() {}
 }
 
-struct Trinsic_Services_ListCredentialTemplatesRequest {
+public struct Trinsic_Services_ListCredentialTemplatesRequest {
   // SwiftProtobuf.Message conformance is added in an extension below. See the
   // `Message` and `Message+*Additions` files in the SwiftProtobuf library for
   // methods supported on all messages.
 
-  var query: String = String()
+  public var query: String = String()
 
-  var unknownFields = SwiftProtobuf.UnknownStorage()
+  public var unknownFields = SwiftProtobuf.UnknownStorage()
 
-  init() {}
+  public init() {}
 }
 
-struct Trinsic_Services_ListCredentialTemplatesResponse {
+public struct Trinsic_Services_ListCredentialTemplatesResponse {
   // SwiftProtobuf.Message conformance is added in an extension below. See the
   // `Message` and `Message+*Additions` files in the SwiftProtobuf library for
   // methods supported on all messages.
 
-  var templates: [Trinsic_Services_CredentialTemplate] = []
+  public var templates: [Trinsic_Services_CredentialTemplate] = []
 
-  var unknownFields = SwiftProtobuf.UnknownStorage()
+  public var unknownFields = SwiftProtobuf.UnknownStorage()
 
-  init() {}
+  public init() {}
 }
 
-struct Trinsic_Services_CredentialTemplate {
+public struct Trinsic_Services_CredentialTemplate {
   // SwiftProtobuf.Message conformance is added in an extension below. See the
   // `Message` and `Message+*Additions` files in the SwiftProtobuf library for
   // methods supported on all messages.
 
-  var id: String = String()
+  public var id: String = String()
 
-  var title: String = String()
+  public var title: String = String()
 
-  var description_p: String = String()
+  public var description_p: String = String()
 
-  var contexts: [String] = []
+  public var contexts: [String] = []
 
-  var schema: String = String()
+  public var schema: String = String()
 
-  var unknownFields = SwiftProtobuf.UnknownStorage()
+  public var unknownFields = SwiftProtobuf.UnknownStorage()
 
-  init() {}
+  public init() {}
 }
 
 // MARK: - Code below here is support for the SwiftProtobuf runtime.
@@ -89,50 +89,50 @@ struct Trinsic_Services_CredentialTemplate {
 fileprivate let _protobuf_package = "trinsic.services"
 
 extension Trinsic_Services_CreateCredentialTemplateRequest: SwiftProtobuf.Message, SwiftProtobuf._MessageImplementationBase, SwiftProtobuf._ProtoNameProviding {
-  static let protoMessageName: String = _protobuf_package + ".CreateCredentialTemplateRequest"
-  static let _protobuf_nameMap = SwiftProtobuf._NameMap()
+  public static let protoMessageName: String = _protobuf_package + ".CreateCredentialTemplateRequest"
+  public static let _protobuf_nameMap = SwiftProtobuf._NameMap()
 
-  mutating func decodeMessage<D: SwiftProtobuf.Decoder>(decoder: inout D) throws {
+  public mutating func decodeMessage<D: SwiftProtobuf.Decoder>(decoder: inout D) throws {
     while let _ = try decoder.nextFieldNumber() {
     }
   }
 
-  func traverse<V: SwiftProtobuf.Visitor>(visitor: inout V) throws {
+  public func traverse<V: SwiftProtobuf.Visitor>(visitor: inout V) throws {
     try unknownFields.traverse(visitor: &visitor)
   }
 
-  static func ==(lhs: Trinsic_Services_CreateCredentialTemplateRequest, rhs: Trinsic_Services_CreateCredentialTemplateRequest) -> Bool {
+  public static func ==(lhs: Trinsic_Services_CreateCredentialTemplateRequest, rhs: Trinsic_Services_CreateCredentialTemplateRequest) -> Bool {
     if lhs.unknownFields != rhs.unknownFields {return false}
     return true
   }
 }
 
 extension Trinsic_Services_CreateCredentialTemplateResponse: SwiftProtobuf.Message, SwiftProtobuf._MessageImplementationBase, SwiftProtobuf._ProtoNameProviding {
-  static let protoMessageName: String = _protobuf_package + ".CreateCredentialTemplateResponse"
-  static let _protobuf_nameMap = SwiftProtobuf._NameMap()
+  public static let protoMessageName: String = _protobuf_package + ".CreateCredentialTemplateResponse"
+  public static let _protobuf_nameMap = SwiftProtobuf._NameMap()
 
-  mutating func decodeMessage<D: SwiftProtobuf.Decoder>(decoder: inout D) throws {
+  public mutating func decodeMessage<D: SwiftProtobuf.Decoder>(decoder: inout D) throws {
     while let _ = try decoder.nextFieldNumber() {
     }
   }
 
-  func traverse<V: SwiftProtobuf.Visitor>(visitor: inout V) throws {
+  public func traverse<V: SwiftProtobuf.Visitor>(visitor: inout V) throws {
     try unknownFields.traverse(visitor: &visitor)
   }
 
-  static func ==(lhs: Trinsic_Services_CreateCredentialTemplateResponse, rhs: Trinsic_Services_CreateCredentialTemplateResponse) -> Bool {
+  public static func ==(lhs: Trinsic_Services_CreateCredentialTemplateResponse, rhs: Trinsic_Services_CreateCredentialTemplateResponse) -> Bool {
     if lhs.unknownFields != rhs.unknownFields {return false}
     return true
   }
 }
 
 extension Trinsic_Services_ListCredentialTemplatesRequest: SwiftProtobuf.Message, SwiftProtobuf._MessageImplementationBase, SwiftProtobuf._ProtoNameProviding {
-  static let protoMessageName: String = _protobuf_package + ".ListCredentialTemplatesRequest"
-  static let _protobuf_nameMap: SwiftProtobuf._NameMap = [
+  public static let protoMessageName: String = _protobuf_package + ".ListCredentialTemplatesRequest"
+  public static let _protobuf_nameMap: SwiftProtobuf._NameMap = [
     1: .same(proto: "query"),
   ]
 
-  mutating func decodeMessage<D: SwiftProtobuf.Decoder>(decoder: inout D) throws {
+  public mutating func decodeMessage<D: SwiftProtobuf.Decoder>(decoder: inout D) throws {
     while let fieldNumber = try decoder.nextFieldNumber() {
       // The use of inline closures is to circumvent an issue where the compiler
       // allocates stack space for every case branch when no optimizations are
@@ -144,14 +144,14 @@ extension Trinsic_Services_ListCredentialTemplatesRequest: SwiftProtobuf.Message
     }
   }
 
-  func traverse<V: SwiftProtobuf.Visitor>(visitor: inout V) throws {
+  public func traverse<V: SwiftProtobuf.Visitor>(visitor: inout V) throws {
     if !self.query.isEmpty {
       try visitor.visitSingularStringField(value: self.query, fieldNumber: 1)
     }
     try unknownFields.traverse(visitor: &visitor)
   }
 
-  static func ==(lhs: Trinsic_Services_ListCredentialTemplatesRequest, rhs: Trinsic_Services_ListCredentialTemplatesRequest) -> Bool {
+  public static func ==(lhs: Trinsic_Services_ListCredentialTemplatesRequest, rhs: Trinsic_Services_ListCredentialTemplatesRequest) -> Bool {
     if lhs.query != rhs.query {return false}
     if lhs.unknownFields != rhs.unknownFields {return false}
     return true
@@ -159,12 +159,12 @@ extension Trinsic_Services_ListCredentialTemplatesRequest: SwiftProtobuf.Message
 }
 
 extension Trinsic_Services_ListCredentialTemplatesResponse: SwiftProtobuf.Message, SwiftProtobuf._MessageImplementationBase, SwiftProtobuf._ProtoNameProviding {
-  static let protoMessageName: String = _protobuf_package + ".ListCredentialTemplatesResponse"
-  static let _protobuf_nameMap: SwiftProtobuf._NameMap = [
+  public static let protoMessageName: String = _protobuf_package + ".ListCredentialTemplatesResponse"
+  public static let _protobuf_nameMap: SwiftProtobuf._NameMap = [
     1: .same(proto: "templates"),
   ]
 
-  mutating func decodeMessage<D: SwiftProtobuf.Decoder>(decoder: inout D) throws {
+  public mutating func decodeMessage<D: SwiftProtobuf.Decoder>(decoder: inout D) throws {
     while let fieldNumber = try decoder.nextFieldNumber() {
       // The use of inline closures is to circumvent an issue where the compiler
       // allocates stack space for every case branch when no optimizations are
@@ -176,14 +176,14 @@ extension Trinsic_Services_ListCredentialTemplatesResponse: SwiftProtobuf.Messag
     }
   }
 
-  func traverse<V: SwiftProtobuf.Visitor>(visitor: inout V) throws {
+  public func traverse<V: SwiftProtobuf.Visitor>(visitor: inout V) throws {
     if !self.templates.isEmpty {
       try visitor.visitRepeatedMessageField(value: self.templates, fieldNumber: 1)
     }
     try unknownFields.traverse(visitor: &visitor)
   }
 
-  static func ==(lhs: Trinsic_Services_ListCredentialTemplatesResponse, rhs: Trinsic_Services_ListCredentialTemplatesResponse) -> Bool {
+  public static func ==(lhs: Trinsic_Services_ListCredentialTemplatesResponse, rhs: Trinsic_Services_ListCredentialTemplatesResponse) -> Bool {
     if lhs.templates != rhs.templates {return false}
     if lhs.unknownFields != rhs.unknownFields {return false}
     return true
@@ -191,8 +191,8 @@ extension Trinsic_Services_ListCredentialTemplatesResponse: SwiftProtobuf.Messag
 }
 
 extension Trinsic_Services_CredentialTemplate: SwiftProtobuf.Message, SwiftProtobuf._MessageImplementationBase, SwiftProtobuf._ProtoNameProviding {
-  static let protoMessageName: String = _protobuf_package + ".CredentialTemplate"
-  static let _protobuf_nameMap: SwiftProtobuf._NameMap = [
+  public static let protoMessageName: String = _protobuf_package + ".CredentialTemplate"
+  public static let _protobuf_nameMap: SwiftProtobuf._NameMap = [
     1: .same(proto: "Id"),
     2: .same(proto: "Title"),
     3: .same(proto: "Description"),
@@ -200,7 +200,7 @@ extension Trinsic_Services_CredentialTemplate: SwiftProtobuf.Message, SwiftProto
     5: .same(proto: "Schema"),
   ]
 
-  mutating func decodeMessage<D: SwiftProtobuf.Decoder>(decoder: inout D) throws {
+  public mutating func decodeMessage<D: SwiftProtobuf.Decoder>(decoder: inout D) throws {
     while let fieldNumber = try decoder.nextFieldNumber() {
       // The use of inline closures is to circumvent an issue where the compiler
       // allocates stack space for every case branch when no optimizations are
@@ -216,7 +216,7 @@ extension Trinsic_Services_CredentialTemplate: SwiftProtobuf.Message, SwiftProto
     }
   }
 
-  func traverse<V: SwiftProtobuf.Visitor>(visitor: inout V) throws {
+  public func traverse<V: SwiftProtobuf.Visitor>(visitor: inout V) throws {
     if !self.id.isEmpty {
       try visitor.visitSingularStringField(value: self.id, fieldNumber: 1)
     }
@@ -235,7 +235,7 @@ extension Trinsic_Services_CredentialTemplate: SwiftProtobuf.Message, SwiftProto
     try unknownFields.traverse(visitor: &visitor)
   }
 
-  static func ==(lhs: Trinsic_Services_CredentialTemplate, rhs: Trinsic_Services_CredentialTemplate) -> Bool {
+  public static func ==(lhs: Trinsic_Services_CredentialTemplate, rhs: Trinsic_Services_CredentialTemplate) -> Bool {
     if lhs.id != rhs.id {return false}
     if lhs.title != rhs.title {return false}
     if lhs.description_p != rhs.description_p {return false}
