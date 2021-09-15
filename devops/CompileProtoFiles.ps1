b19e31f630410f36c79e356acc70c3d4d8227488
Scott Phillips <polygonguru@gmail.com>
2021-09-15 14:53:22 -0400

    Remove merge markers

--- devops/CompileProtoFiles.ps1
+++ devops/CompileProtoFiles.ps1
@@ -54,19 +54,11 @@ function Set-Replace-Lines($filename)
     Set-Content -path $filename -value $fileLines
 }
 
-<<<<<<< HEAD
-function Compile-Golang()
-{
-    $GoPath = "../go/proto"
-    Remove-Old-Proto-Files($GoPath)
-    protoc $( Get-Proto-Path ) `
-=======
 function Update-Golang()
 {
     $GoPath = "../go/proto"
     Remove-OldProtoFiles($GoPath)
     protoc $( Get-ProtoPath ) `
->>>>>>> main
         --go_out="$GoPath" `
         --go-grpc_out="$GoPath" `
         --go_opt=paths=source_relative `
@@ -80,21 +72,12 @@ function Update-Golang()
     Remove-Item -Path "$GoPath/models" -Force -Recurse
 }
 
-<<<<<<< HEAD
-function Compile-Ruby()
-{
-    # TODO - Get this plugin path directly.
-    $RubyPath = "../ruby/lib/trinsic"
-    Remove-Old-Proto-Files($RubyPath)
-    grpc_tools_ruby_protoc $( Get-Proto-Path ) `
-=======
 function Update-Ruby()
 {
     # TODO - Get this plugin path directly.
     $RubyPath = "../ruby/lib/trinsic"
     Remove-OldProtoFiles($RubyPath)
     grpc_tools_ruby_protoc $( Get-ProtoPath ) `
->>>>>>> main
        --ruby_out="$RubyPath" `
        --grpc_out="$RubyPath" `
        $( Get-ProtoFiles )
@@ -117,12 +100,6 @@ function Update-Ruby()
 
 function Update-Swift()
 {
-<<<<<<< HEAD
-    protoc $( Get-Proto-Path ) `
-        --swift_out=./Sources/OkapiSwift/proto `
-        --swift_opt=Visibility=Public  `
-        $( Get-Proto-Files )
-=======
     $SwiftPath = "../swift/TrinsicServices/Sources/TrinsicServices/proto"
     Remove-OldProtoFiles($SwiftPath)
     protoc $( Get-ProtoPath ) `
@@ -130,25 +107,11 @@ function Update-Swift()
         --swift_out="$SwiftPath" `
         --grpc-swift_out="$SwiftPath" `
         $( Get-ProtoFiles )
->>>>>>> main
 }
 
 function Update-Java()
 {
     $JavaPath = "../java/src/main/java"
-<<<<<<< HEAD
-    protoc $( Get-Proto-Path ) `
-        --plugin=$( Get-Java-Plugin-Path ) `
-        --java_out="$JavaPath" `
-        $( Get-Proto-Files )
-}
-
-Setup
-Compile-Golang
-Compile-Ruby
-Compile-Swift
-Compile-Java
-=======
     protoc $( Get-ProtoPath ) `
         --plugin=$( Get-JavaPluginPath ) `
         --java_out="$JavaPath" `
@@ -160,6 +123,5 @@ Update-Golang
 Update-Ruby
 Update-Swift
 Update-Java
->>>>>>> main
 # Python is handled in the BuildPython due to venv requirements
 . "./BuildPython.ps1" -RequirementsOnly $true
