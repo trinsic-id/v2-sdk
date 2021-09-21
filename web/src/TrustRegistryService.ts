cd7b492faf44bc21386a7ee77ef41f15d2e851ad
Tomislav Markovski <tomislav@trinsic.id>
2021-09-21 13:14:30 -0400

    trustregistry service for ts web package

---
+++ web/src/TrustRegistryService.ts
@@ -0,0 +1,118 @@
+import ServiceBase from "./ServiceBase";
+import { TrustRegistryClient } from "./proto/TrustRegistryServiceClientPb";
+import {
+  AddFrameworkRequest,
+  AddFrameworkResponse,
+  CheckIssuerStatusRequest,
+  CheckIssuerStatusResponse,
+  CheckVerifierStatusRequest,
+  CheckVerifierStatusResponse,
+  RegisterIssuerRequest,
+  RegisterIssuerResponse,
+  RegisterVerifierRequest,
+  RegisterVerifierResponse,
+  RemoveFrameworkRequest,
+  RemoveFrameworkResponse,
+  SearchRegistryRequest,
+  SearchRegistryResponse,
+  UnregisterMemberRequest,
+  UnregisterMemberResponse,
+} from "./proto/TrustRegistry_pb";
+
+export class TrustRegistryService extends ServiceBase {
+  client: TrustRegistryClient;
+
+  constructor(serviceAddress: string = "localhost:5000") {
+    super();
+
+    this.client = new TrustRegistryClient(serviceAddress, null, null);
+  }
+
+  public registerIssuer(request: RegisterIssuerRequest): Promise<RegisterIssuerResponse> {
+    return new Promise((resolve, reject) => {
+      this.client.registerIssuer(request, this.getMetadata(), (error, response) => {
+        if (error) {
+          reject(error);
+        }
+        return resolve(response);
+      });
+    });
+  }
+
+  public registerVerifier(request: RegisterVerifierRequest): Promise<RegisterVerifierResponse> {
+    return new Promise((resolve, reject) => {
+      this.client.registerVerifier(request, this.getMetadata(), (error, response) => {
+        if (error) {
+          reject(error);
+        }
+        return resolve(response);
+      });
+    });
+  }
+
+  public unregisterMember(request: UnregisterMemberRequest): Promise<UnregisterMemberResponse> {
+    return new Promise((resolve, reject) => {
+      this.client.unregisterMember(request, this.getMetadata(), (error, response) => {
+        if (error) {
+          reject(error);
+        }
+        return resolve(response);
+      });
+    });
+  }
+
+  public checkIssuer(request: CheckIssuerStatusRequest): Promise<CheckIssuerStatusResponse> {
+    return new Promise((resolve, reject) => {
+      this.client.checkIssuerStatus(request, this.getMetadata(), (error, response) => {
+        if (error) {
+          reject(error);
+        }
+        return resolve(response);
+      });
+    });
+  }
+
+  public checkVerifier(request: CheckVerifierStatusRequest): Promise<CheckVerifierStatusResponse> {
+    return new Promise((resolve, reject) => {
+      this.client.checkVerifierStatus(request, this.getMetadata(), (error, response) => {
+        if (error) {
+          reject(error);
+        }
+        return resolve(response);
+      });
+    });
+  }
+
+  public search(query: string): Promise<SearchRegistryResponse> {
+    return new Promise((resolve, reject) => {
+      this.client.searchRegistry(new SearchRegistryRequest().setQuery(query), this.getMetadata(), (error, response) => {
+        if (error) {
+          reject(error);
+        }
+        return resolve(response);
+      });
+    });
+  }
+
+  public addGovernanceFramework(request: AddFrameworkRequest): Promise<AddFrameworkResponse> {
+    return new Promise((resolve, reject) => {
+      this.client.addFramework(request, this.getMetadata(), (error, response) => {
+        if (error) {
+          reject(error);
+        }
+        return resolve(response);
+      });
+    });
+  }
+
+  public removeGovernanceFramework(request: RemoveFrameworkRequest): Promise<RemoveFrameworkResponse> {
+    return new Promise((resolve, reject) => {
+      this.client.removeFramework(request, this.getMetadata(), (error, response) => {
+        if (error) {
+          reject(error);
+        }
+        return resolve(response);
+      });
+    });
+  }
+}
