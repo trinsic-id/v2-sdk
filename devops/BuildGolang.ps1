646891bc5a84397a1f7e9ee7af039daf661d0a9c
Scott Phillips <polygonguru@gmail.com>
2021-09-21 21:59:21 -0400

    Copy to gomod

--- devops/BuildGolang.ps1
+++ devops/BuildGolang.ps1
@@ -1,15 +1,19 @@
 param
 (
+    [AllowNull()][string]$ArtifactName = 'windows-gnu',
     [AllowNull()][string]$GitTag = '',
     [AllowNull()][string]$PackageVersion = '',
     [AllowNull()][string]$TestOutput = 'test_output.xml',
-    [AllowNull()][string]$ArtifactName = '',
     [AllowNull()][Boolean]$RequirementsOnly = $false
 )
 
 . "$PSScriptRoot/VersionParse.ps1"
 
 function Install-Requirements {
+    $GOMODPATH = "$env:GOPATH/pkg/mod/github.com/trinsic-id/okapi"
+    Get-ChildItem $GOMODPATH | `
+        Where-Object {$_.PSIsContainer -eq $True } | `
+        ForEach-Object {Copy-NativeBinaries -Destination "$($_.FullName)/okapi"}
 }
 function Test-Golang {
     # go test -v 2>&1 | go-junit-report > $TestOutput
@@ -25,15 +29,18 @@ function Build-Package {
     }
 }
 
+function Copy-NativeBinaries($Destination) {
+    $source = "$PSScriptRoot/../libs/$ArtifactName"
+    Get-ChildItem $source -Recurse | `
+        Where-Object { $_.PSIsContainer -eq $False } | `
+        ForEach-Object {Copy-Item -Path $_.Fullname -Destination $Destination -Force} # Do the things
+    Copy-Item -Path "$source/../C_header/okapi.h" -Destination "$Destination" -Force
+}
+
 # Setup
 $InvocationPath = (Get-Item .).FullName
 Set-Location "$PSScriptRoot/../go"
-$source = "../libs/$ArtifactName"
-$dest = "./services"
-Get-ChildItem $source -Recurse | `
-    Where-Object { $_.PSIsContainer -eq $False } | `
-    ForEach-Object {Copy-Item -Path $_.Fullname -Destination $dest -Force} # Do the things
-Copy-Item -Path "../libs/C_header/okapi.h" -Destination "$dest"
+Copy-NativeBinaries -Destination "./services"
 Install-Requirements
 if (!$RequirementsOnly) {
     cd "./services"
