83ead1ebf820942d9d07d918912c1455be4d55ba
Tomislav Markovski <tomislav@trinsic.id>
2021-10-22 09:58:42 -0400

    Test fixes

--- web/test/ProviderService.test.js
+++ web/test/ProviderService.test.js
@@ -1,26 +1,24 @@
 const { ProviderService, WalletService, InviteRequest } = require("../lib");
-let endpoint = require('./env').env.ENDPOINT
+let endpoint = require("./env").env.ENDPOINT;
 
 const randomEmail = (suffix = "example.com", length = 16) => {
-  return Math.random().toString(16).substr(2, length) + '@' + suffix;
-}
+  return Math.random().toString(16).substr(2, length) + "@" + suffix;
+};
 
 const createProfile = async () => {
   // if you don't have a profile saved
   let service = new WalletService(endpoint);
-  let profile = await service.createWallet()
+  let profile = await service.createWallet();
 
   return profile;
-}
+};
 
 it("make an invitation", async () => {
   let providerService = new ProviderService(endpoint);
   let profile = await createProfile();
   await providerService.setProfile(profile);
-  let inviteRequest = new InviteRequest();
-  inviteRequest.setEmail(randomEmail());
-  inviteRequest.setDescription("invitation");
-  
+  let inviteRequest = new InviteRequest().setEmail(randomEmail()).setDescription("invitation");
+
   let inviteResponse = await providerService.inviteParticipant(inviteRequest);
 
   expect(inviteResponse).not.toBeNull();
