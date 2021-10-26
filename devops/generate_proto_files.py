81ebf07d3b819964a64ef43a1011e4b83345ed50
Scott Phillips <polygonguru@gmail.com>
2021-10-26 12:35:14 -0400

    SDK-147: use python for compiling proto files, golang context support (#155)

---
+++ devops/generate_proto_files.py
@@ -0,0 +1,98 @@
+import glob
+import os
+import shutil
+from os.path import abspath, join, dirname
+from typing import List, Dict
+
+
+def get_language_dir(language_name: str) -> str:
+    return abspath(join(dirname(abspath(__file__)), '..', language_name))
+
+
+def get_proto_files(dir_name: str = None) -> List[str]:
+    dir_name = dir_name or get_language_dir('proto')
+    proto_search_glob = join(dir_name, '**', '*.proto')
+    return [abspath(file_path) for file_path in glob.glob(proto_search_glob, recursive=True)]
+
+
+def replace_lines_in_file(file_name: str, replace_lines: Dict[str, str]) -> None:
+    with open(file_name, 'r') as fid:
+        file_lines = fid.readlines()
+
+    file_lines = [line.replace(key, value) for key, value in replace_lines.items() for line in file_lines]
+
+    with open(file_name, 'w') as fid:
+        fid.writelines(file_lines)
+
+
+def clean_proto_dir(language_proto_dir: str) -> None:
+    try:
+        shutil.rmtree(language_proto_dir)
+    except:
+        pass
+    os.mkdir(language_proto_dir)
+
+
+def join_args(args: Dict[str, str]) -> str:
+    return " ".join([f'--{key}="{value}"' for (key, value) in args.items()]) if args else ''
+
+
+def run_protoc(language_options: Dict[str, str] = None,
+               custom_options: Dict[str, str] = None,
+               proto_files: List[str] = None,
+               plugin: str = None,
+               protoc_executable: str = 'protoc') -> None:
+    language_arg_string = join_args(language_options)
+    proto_path_string = f'--proto_path="{get_language_dir("proto")}"'
+    custom_options_string = join_args(custom_options)
+    proto_files_string = " ".join(proto_files)
+    plugin_string = f'--plugin={plugin}' if plugin else ''
+    protoc_command = f'{protoc_executable} {plugin_string} {proto_path_string} {language_arg_string} {custom_options_string} {proto_files_string}'
+    print(protoc_command)
+    if os.system(protoc_command) != 0:
+        raise Exception("Protoc failed")
+
+
+def update_golang():
+    go_path = get_language_dir('go')
+    go_proto_path = join(go_path, 'proto')
+    clean_proto_dir(go_proto_path)
+    run_protoc({'go_out': go_proto_path, 'go-grpc_out': go_proto_path},
+               {'go_opt': 'module=github.com/trinsic-id/sdk', 'go-grpc_opt': 'module=github.com/trinsic-id/sdk'},
+               get_proto_files())
+    # Remove okapi proto folder
+    shutil.rmtree(join(go_proto_path, 'go'))
+    # find and replace the sdk proto with okapi proto
+    replace_pairs = {'okapiproto "github.com/trinsic-id/sdk/go/okapiproto"':
+                     'okapiproto "github.com/trinsic-id/okapi/go/okapiproto"'}
+    for file_name in glob.glob(join(go_proto_path, '*.go')):
+        replace_lines_in_file(file_name, replace_pairs)
+
+
+def update_ruby():
+    ruby_path = get_language_dir('ruby')
+    ruby_proto_path = join(ruby_path, 'lib')
+    # TODO - clean selectively
+    run_protoc({'ruby_out': ruby_proto_path, 'grpc_out': ruby_proto_path}, {}, get_proto_files(),
+               protoc_executable='grpc_tools_ruby_protoc')
+    # TODO - Ruby type specifications
+
+
+def update_java():
+    java_path = get_language_dir('java')
+    java_proto_path = join(java_path, 'src', 'main', 'java')
+    # TODO - clean_proto_dir(java_proto_path)
+    run_protoc({'java_out': java_proto_path}, {}, get_proto_files(),
+               plugin=r"C:\bin\protoc-gen-grpc-java-1.39.0-windows-x86_64.exe")
+    # remove okapi pbmse
+    shutil.rmtree(join(java_proto_path, 'trinsic', 'okapi'))
+
+
+def main():
+    update_golang()
+    update_ruby()
+    update_java()
+
+
+if __name__ == "__main__":
+    main()
--- go/services/services.go
+++ go/services/services.go
@@ -25,17 +25,20 @@ type ServiceBase struct {
 }
 
 type Service interface {
-	GetContext() (context.Context, error)
+	GetMetadataContext(userContext context.Context) (context.Context, error)
 	GetMetadata() (metadata.MD, error)
 	SetProfile(profile *sdk.WalletProfile) error
 }
 
-func (s *ServiceBase) GetContext() (context.Context, error) {
+func (s *ServiceBase) GetMetadataContext(userContext context.Context) (context.Context, error) {
 	md, err := s.GetMetadata()
 	if err != nil {
 		return nil, err
 	}
-	return metadata.NewOutgoingContext(context.Background(), md), nil
+	if userContext == nil {
+		return nil, errors.New("userContext cannot be nil")
+	}
+	return metadata.NewOutgoingContext(userContext, md), nil
 }
 
 func (s *ServiceBase) GetMetadata() (metadata.MD, error) {
@@ -87,14 +90,14 @@ func (s *ServiceBase) SetProfile(profile *sdk.WalletProfile) error {
 
 type WalletService interface {
 	Service
-	RegisterOrConnect(email string) error
-	CreateWallet(securityCode string) (*sdk.WalletProfile, error)
-	IssueCredential(document Document) (Document, error)
-	Search(query string) (*sdk.SearchResponse, error)
-	InsertItem(item Document) (string, error)
-	Send(document Document, email string) error
-	CreateProof(documentId string, revealDocument Document) (Document, error)
-	VerifyProof(proofDocument Document) (bool, error)
+	RegisterOrConnect(userContext context.Context, email string) error
+	CreateWallet(userContext context.Context, securityCode string) (*sdk.WalletProfile, error)
+	IssueCredential(userContext context.Context, document Document) (Document, error)
+	Search(userContext context.Context, query string) (*sdk.SearchResponse, error)
+	InsertItem(userContext context.Context, item Document) (string, error)
+	Send(userContext context.Context, document Document, email string) error
+	CreateProof(userContext context.Context, documentId string, revealDocument Document) (Document, error)
+	VerifyProof(userContext context.Context, proofDocument Document) (bool, error)
 }
 
 func CreateWalletService(serviceAddress string, channel *grpc.ClientConn) (WalletService, error) {
@@ -150,12 +153,12 @@ type WalletBase struct {
 	credentialClient sdk.CredentialClient
 }
 
-func (w *WalletBase) RegisterOrConnect(email string) error {
+func (w *WalletBase) RegisterOrConnect(userContext context.Context, email string) error {
 	connectRequest := sdk.ConnectRequest{
 		ContactMethod: &sdk.ConnectRequest_Email{Email: email},
 	}
 
-	md, err := w.GetContext()
+	md, err := w.GetMetadataContext(userContext)
 	if err != nil {
 		return err
 	}
@@ -166,99 +169,40 @@ func (w *WalletBase) RegisterOrConnect(email string) error {
 	return nil
 }
 
-func (w *WalletBase) CreateWallet(securityCode string) (*sdk.WalletProfile, error) {
-	configuration, err := w.walletClient.GetProviderConfiguration(context.Background(), &sdk.GetProviderConfigurationRequest{})
-	if err != nil {
-		return nil, err
-	}
-
+func (w *WalletBase) CreateWallet(userContext context.Context, securityCode string) (*sdk.WalletProfile, error) {
 	dk := okapi.DidKey()
-	dc := okapi.DidComm()
 
-	resolveResponse, err := dk.Resolve(&okapiproto.ResolveRequest{Did: configuration.KeyAgreementKeyId})
-	if err != nil {
-		return nil, err
-	}
-
-	var providerExchangeKey *okapiproto.JsonWebKey
-	for _, key := range resolveResponse.Keys {
-		if key.Kid == configuration.KeyAgreementKeyId {
-			providerExchangeKey = key
-			break
-		}
-	}
-
+	// Generate new DID used by the current device
 	myKey, err := dk.Generate(&okapiproto.GenerateKeyRequest{KeyType: okapiproto.KeyType_KEY_TYPE_ED25519})
 	if err != nil {
 		return nil, err
 	}
 
-	var myExchangeKey *okapiproto.JsonWebKey
-	for _, key := range myKey.Key {
-		if key.Crv == "X25519" {
-			myExchangeKey = key
-			break
-		}
-	}
-
 	myDidDocument := myKey.DidDocument.AsMap()
-	walletRequest := sdk.CreateWalletRequest{
+	walletRequest := &sdk.CreateWalletRequest{
 		Controller:   myDidDocument["id"].(string),
-		Description:  "My Cloud Wallet",
 		SecurityCode: securityCode,
 	}
-	walletBytes, err := proto.Marshal(&walletRequest)
-	if err != nil {
-		return nil, err
-	}
-	packedMessage, err := dc.Pack(&okapiproto.PackRequest{
-		SenderKey:   myExchangeKey,
-		ReceiverKey: providerExchangeKey,
-		Plaintext:   walletBytes,
-	})
+
+	createWalletResponse, err := w.walletClient.CreateWallet(userContext, walletRequest)
 	if err != nil {
 		return nil, err
 	}
 
-	packedSdkMessage := packedMessage.Message
-	response, err := w.walletClient.CreateWalletEncrypted(context.Background(), packedSdkMessage)
-	if err != nil {
-		return nil, err
-	}
-
-	decryptedResponse, err := dc.Unpack(&okapiproto.UnpackRequest{
-		SenderKey:   providerExchangeKey,
-		ReceiverKey: myExchangeKey,
-		Message:     response,
-	})
-	if err != nil {
-		return nil, err
-	}
-
-	createWalletResponse := sdk.CreateWalletResponse{}
-	err = proto.Unmarshal(decryptedResponse.Plaintext, &createWalletResponse)
-	if err != nil {
-		return nil, err
-	}
-
-	myKeyJwk, err := proto.Marshal(myKey.Key[0])
-	if err != nil {
-		return nil, err
-	}
-	jsonString, err := json.Marshal(myDidDocument)
+	keyBytes, err := proto.Marshal(myKey.Key[0])
 	if err != nil {
 		return nil, err
 	}
 	return &sdk.WalletProfile{
-		DidDocument: &sdk.JsonPayload{Json: &sdk.JsonPayload_JsonString{JsonString: string(jsonString)}},
+		DidDocument: &sdk.JsonPayload{Json: &sdk.JsonPayload_JsonStruct{JsonStruct: myKey.DidDocument}},
 		WalletId:    createWalletResponse.WalletId,
 		Invoker:     createWalletResponse.Invoker,
 		Capability:  createWalletResponse.Capability,
-		InvokerJwk:  myKeyJwk,
+		InvokerJwk:  keyBytes,
 	}, nil
 }
 
-func (w *WalletBase) IssueCredential(document Document) (Document, error) {
+func (w *WalletBase) IssueCredential(userContext context.Context, document Document) (Document, error) {
 	jsonBytes, err := json.Marshal(document)
 	if err != nil {
 		return nil, err
@@ -271,7 +215,7 @@ func (w *WalletBase) IssueCredential(document Document) (Document, error) {
 		},
 	}
 
-	md, err := w.GetContext()
+	md, err := w.GetMetadataContext(userContext)
 	if err != nil {
 		return nil, err
 	}
@@ -287,8 +231,8 @@ func (w *WalletBase) IssueCredential(document Document) (Document, error) {
 	return doc, nil
 }
 
-func (w *WalletBase) Search(query string) (*sdk.SearchResponse, error) {
-	md, err := w.GetContext()
+func (w *WalletBase) Search(userContext context.Context, query string) (*sdk.SearchResponse, error) {
+	md, err := w.GetMetadataContext(userContext)
 	if err != nil {
 		return nil, err
 	}
@@ -301,12 +245,12 @@ func (w *WalletBase) Search(query string) (*sdk.SearchResponse, error) {
 	return response, nil
 }
 
-func (w *WalletBase) InsertItem(item Document) (string, error) {
+func (w *WalletBase) InsertItem(userContext context.Context, item Document) (string, error) {
 	jsonString, err := json.Marshal(item)
 	if err != nil {
 		return "", err
 	}
-	md, err := w.GetContext()
+	md, err := w.GetMetadataContext(userContext)
 	if err != nil {
 		return "", err
 	}
@@ -323,12 +267,12 @@ func (w *WalletBase) InsertItem(item Document) (string, error) {
 	return response.ItemId, nil
 }
 
-func (w *WalletBase) Send(document Document, email string) error {
+func (w *WalletBase) Send(userContext context.Context, document Document, email string) error {
 	jsonString, err := json.Marshal(document)
 	if err != nil {
 		return err
 	}
-	md, err := w.GetContext()
+	md, err := w.GetMetadataContext(userContext)
 	if err != nil {
 		return err
 	}
@@ -348,12 +292,12 @@ func (w *WalletBase) Send(document Document, email string) error {
 	return nil
 }
 
-func (w *WalletBase) CreateProof(documentId string, revealDocument Document) (Document, error) {
+func (w *WalletBase) CreateProof(userContext context.Context, documentId string, revealDocument Document) (Document, error) {
 	jsonString, err := json.Marshal(revealDocument)
 	if err != nil {
 		return nil, err
 	}
-	md, err := w.GetContext()
+	md, err := w.GetMetadataContext(userContext)
 	if err != nil {
 		return nil, err
 	}
@@ -376,12 +320,12 @@ func (w *WalletBase) CreateProof(documentId string, revealDocument Document) (Do
 	return proofMap, nil
 }
 
-func (w *WalletBase) VerifyProof(proofDocument Document) (bool, error) {
+func (w *WalletBase) VerifyProof(userContext context.Context, proofDocument Document) (bool, error) {
 	jsonString, err := json.Marshal(proofDocument)
 	if err != nil {
 		return false, err
 	}
-	md, err := w.GetContext()
+	md, err := w.GetMetadataContext(userContext)
 	if err != nil {
 		return false, err
 	}
@@ -400,8 +344,8 @@ func (w *WalletBase) VerifyProof(proofDocument Document) (bool, error) {
 
 type ProviderService interface {
 	Service
-	InviteParticipant(request *sdk.InviteRequest) (*sdk.InviteResponse, error)
-	InvitationStatus(request *sdk.InvitationStatusRequest) (*sdk.InvitationStatusResponse, error)
+	InviteParticipant(userContext context.Context, request *sdk.InviteRequest) (*sdk.InviteResponse, error)
+	InvitationStatus(userContext context.Context, request *sdk.InvitationStatusRequest) (*sdk.InvitationStatusResponse, error)
 }
 
 type ProviderBase struct {
@@ -424,21 +368,21 @@ func CreateProviderService(serviceAddress string, channel *grpc.ClientConn) (Pro
 	return service, nil
 }
 
-func (p *ProviderBase) InviteParticipant(request *sdk.InviteRequest) (*sdk.InviteResponse, error) {
+func (p *ProviderBase) InviteParticipant(userContext context.Context, request *sdk.InviteRequest) (*sdk.InviteResponse, error) {
 	// Verify contact method is set
 	switch request.ContactMethod.(type) {
 	case nil:
 		return nil, fmt.Errorf("unset contact method")
 	}
-	response, err := p.providerClient.Invite(context.Background(), request)
+	response, err := p.providerClient.Invite(userContext, request)
 	if err != nil {
 		return nil, err
 	}
 	return response, nil
 }
 
-func (p *ProviderBase) InvitationStatus(request *sdk.InvitationStatusRequest) (*sdk.InvitationStatusResponse, error) {
-	response, err := p.providerClient.InvitationStatus(context.Background(), request)
+func (p *ProviderBase) InvitationStatus(userContext context.Context, request *sdk.InvitationStatusRequest) (*sdk.InvitationStatusResponse, error) {
+	response, err := p.providerClient.InvitationStatus(userContext, request)
 	if err != nil {
 		return nil, err
 	}
