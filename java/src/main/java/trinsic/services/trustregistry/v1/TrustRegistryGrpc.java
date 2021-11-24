a6848883bf5f980b682d57ccfaedc9ce8d0a1c2d
Scott Phillips <polygonguru@gmail.com>
2021-11-24 13:25:36 -0500

    Java updates

--- java/src/main/java/trinsic/services/trustregistry/v1/TrustRegistryGrpc.java
+++ java/src/main/java/trinsic/services/trustregistry/v1/TrustRegistryGrpc.java
@@ -5,7 +5,7 @@ import static io.grpc.MethodDescriptor.generateFullMethodName;
 /**
  */
 @javax.annotation.Generated(
-    value = "by gRPC proto compiler (version 1.41.0)",
+    value = "by gRPC proto compiler (version 1.42.1)",
     comments = "Source: services/trust-registry/v1/trust-registry.proto")
 @io.grpc.stub.annotations.GrpcGenerated
 public final class TrustRegistryGrpc {
