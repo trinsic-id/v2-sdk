3af5e074ad8034ab7818e70ca76a34a6d6ade77d
Scott Phillips <polygonguru@gmail.com>
2021-12-16 18:46:47 -0500

    Update CredentialsServiceKt.kt

--- java/src/main/java/trinsic/services/CredentialsServiceKt.kt
+++ java/src/main/java/trinsic/services/CredentialsServiceKt.kt
@@ -1,9 +1,19 @@
 package trinsic.services
 
+import com.google.common.base.Function
+import com.google.common.util.concurrent.Futures
+import com.google.common.util.concurrent.ListenableFuture
+import com.google.gson.Gson
+import com.google.protobuf.InvalidProtocolBufferException
 import io.grpc.Channel
+import trinsic.TrinsicUtilities
+import trinsic.okapi.DidException
 import trinsic.services.account.v1.AccountOuterClass
 import trinsic.services.common.v1.CommonOuterClass
+import trinsic.services.verifiablecredentials.v1.VerifiableCredentialGrpc.VerifiableCredentialFutureStub
 import trinsic.services.verifiablecredentials.v1.VerifiableCredentialGrpcKt
+import trinsic.services.verifiablecredentials.v1.VerifiableCredentials.*
+import java.util.concurrent.Executors
 
 class CredentialsServiceKt(
     accountProfile: AccountOuterClass.AccountProfile?,
@@ -11,4 +21,36 @@ class CredentialsServiceKt(
     channel: Channel?
 ) : ServiceBase(accountProfile, serverConfig, channel) {
     var stub = VerifiableCredentialGrpcKt.VerifiableCredentialCoroutineStub(this.channel)
+
+    @Throws(InvalidProtocolBufferException::class, DidException::class)
+    suspend fun issueCredential(document: HashMap<*, *>?): java.util.HashMap<*, *>? {
+        val request = IssueRequest.newBuilder()
+            .setDocument(TrinsicUtilities.createPayloadString(document)).build()
+        return Gson().fromJson(withMetadata(stub, request).issue(request).document.jsonString, HashMap::class.java)
+    }
+
+    @Throws(InvalidProtocolBufferException::class, DidException::class)
+    suspend fun createProof(documentId: String?, revealDocument: HashMap<*, *>?): java.util.HashMap<*, *>? {
+        val request = CreateProofRequest.newBuilder()
+            .setDocumentId(documentId)
+            .setRevealDocument(TrinsicUtilities.createPayloadString(revealDocument))
+            .build()
+        return Gson().fromJson(withMetadata(stub, request).createProof(request).proofDocument.jsonString, HashMap::class.java)
+    }
+
+    @Throws(InvalidProtocolBufferException::class, DidException::class)
+    suspend fun verifyProof(proofDocument: HashMap<*, *>?): Boolean {
+        val request = VerifyProofRequest.newBuilder()
+            .setProofDocument(TrinsicUtilities.createPayloadString(proofDocument)).build()
+        return withMetadata(stub, request).verifyProof(request).valid
+    }
+
+    @Throws(InvalidProtocolBufferException::class, DidException::class)
+    suspend fun send(document: HashMap<*, *>?, email: String?): SendResponse? {
+        val request = SendRequest.newBuilder()
+            .setEmail(email)
+            .setDocument(TrinsicUtilities.createPayloadString(document))
+            .build()
+        return withMetadata(stub, request).send(request)
+    }
 }
