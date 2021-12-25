61327ec77b247cdb0d13e9e44e1af329de41c201
Scott Phillips <polygonguru@gmail.com>
2021-12-24 20:35:28 -0500

    Refactor services to separate files

---
+++ go/services/provider_service.go
@@ -0,0 +1,119 @@
+package services
+
+import (
+	"context"
+	"fmt"
+	sdk "github.com/trinsic-id/sdk/go/proto"
+	"google.golang.org/grpc"
+)
+
+func CreateProviderService(profile *sdk.AccountProfile, serverConfig *sdk.ServerConfig, channel *grpc.ClientConn) (ProviderService, error) {
+	base, err := CreateServiceBase(profile, serverConfig, channel)
+	if err != nil {
+		return nil, err
+	}
+	service := &ProviderBase{
+		ServiceBase: base,
+		client:      sdk.NewProviderClient(base.channel),
+	}
+
+	return service, nil
+}
+
+type ProviderService interface {
+	Service
+	InviteParticipant(userContext context.Context, request *sdk.InviteRequest) (*sdk.InviteResponse, error)
+	InvitationStatus(userContext context.Context, request *sdk.InvitationStatusRequest) (*sdk.InvitationStatusResponse, error)
+	CreateEcosystem(ctx context.Context, request *sdk.CreateEcosystemRequest) (*sdk.CreateEcosystemResponse, error)
+	ListEcosystems(ctx context.Context) ([]*sdk.Ecosystem, error)
+	//AcceptInvite(ctx context.Context, code string) (*sdk.AcceptInviteResponse, error)
+	//SetEcosystem(ecosystemId string)
+}
+
+type ProviderBase struct {
+	*ServiceBase
+	channel *grpc.ClientConn
+	client  sdk.ProviderClient
+}
+
+//func (p *ProviderBase) SetEcosystem(ecosystemId string) {
+//	p.ecosystemId = ecosystemId
+//}
+
+func (p *ProviderBase) InviteParticipant(ctx context.Context, request *sdk.InviteRequest) (*sdk.InviteResponse, error) {
+	// Verify contact method is set
+	switch request.ContactMethod.(type) {
+	case nil:
+		return nil, fmt.Errorf("unset contact method")
+	}
+
+	md, err := p.GetMetadataContext(ctx, request)
+	if err != nil {
+		return nil, err
+	}
+
+	response, err := p.client.Invite(md, request)
+	if err != nil {
+		return nil, err
+	}
+	return response, nil
+}
+
+func (p *ProviderBase) InvitationStatus(ctx context.Context, request *sdk.InvitationStatusRequest) (*sdk.InvitationStatusResponse, error) {
+	md, err := p.GetMetadataContext(ctx, request)
+	if err != nil {
+		return nil, err
+	}
+
+	response, err := p.client.InvitationStatus(md, request)
+	if err != nil {
+		return nil, err
+	}
+
+	return response, nil
+}
+
+func (p *ProviderBase) CreateEcosystem(ctx context.Context, request *sdk.CreateEcosystemRequest) (*sdk.CreateEcosystemResponse, error) {
+	md, err := p.GetMetadataContext(ctx, request)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := p.client.CreateEcosystem(md, request)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+}
+
+func (p *ProviderBase) ListEcosystems(ctx context.Context) ([]*sdk.Ecosystem, error) {
+	request := &sdk.ListEcosystemsRequest{}
+	md, err := p.GetMetadataContext(ctx, request)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := p.client.ListEcosystems(md, request)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp.Ecosystem, nil
+}
+
+//func (p *ProviderBase) AcceptInvite(ctx context.Context, code string) (*sdk.AcceptInviteResponse, error) {
+//	request := &sdk.InfoRequest{}
+//
+//	md, err := p.GetMetadataContext(ctx, request)
+//	if err != nil {
+//		return nil, err
+//	}
+//
+//	response, err := p.client.AcceptInvite(md, &sdk.AcceptInviteRequest{Code: code})
+//	if err != nil {
+//		return nil, err
+//	}
+//
+//	return response, err
+//}
---
+++ go/services/trustregistry_service.go
@@ -0,0 +1,134 @@
+package services
+
+import (
+	"context"
+	sdk "github.com/trinsic-id/sdk/go/proto"
+	"google.golang.org/grpc"
+	"net/url"
+)
+
+func CreateTrustRegistryService(profile *sdk.AccountProfile, serverConfig *sdk.ServerConfig, channel *grpc.ClientConn) (TrustRegistryService, error) {
+	base, err := CreateServiceBase(profile, serverConfig, channel)
+	if err != nil {
+		return nil, err
+	}
+	service := &TrustRegistryBase{
+		ServiceBase: base,
+		client:      sdk.NewTrustRegistryClient(base.channel),
+	}
+
+	return service, nil
+}
+
+type TrustRegistryService interface {
+	Service
+	RegisterGovernanceFramework(userContext context.Context, governanceFramework string, description string) error
+	RegisterIssuer(userContext context.Context, request *sdk.RegisterIssuerRequest) error
+	UnregisterIssuer(userContext context.Context, request *sdk.UnregisterIssuerRequest) error
+	RegisterVerifier(userContext context.Context, request *sdk.RegisterVerifierRequest) error
+	UnregisterVerifier(userContext context.Context, request *sdk.UnregisterVerifierRequest) error
+	CheckIssuerStatus(userContext context.Context, request *sdk.CheckIssuerStatusRequest) (sdk.RegistrationStatus, error)
+	CheckVerifierStatus(userContext context.Context, request *sdk.CheckVerifierStatusRequest) (sdk.RegistrationStatus, error)
+	SearchRegistry(userContext context.Context, query string) (*sdk.SearchRegistryResponse, error)
+}
+
+type TrustRegistryBase struct {
+	*ServiceBase
+	client sdk.TrustRegistryClient
+}
+
+func (t *TrustRegistryBase) RegisterGovernanceFramework(userContext context.Context, governanceFramework string, description string) error {
+	// Verify that it is a valid uri
+	_, err := url.Parse(governanceFramework)
+	if err != nil {
+		return err
+	}
+	request := &sdk.AddFrameworkRequest{
+		GovernanceFramework: &sdk.GovernanceFramework{
+			GovernanceFrameworkUri: governanceFramework,
+			TrustRegistryUri:       "",
+			Description:            description,
+		},
+	}
+	md, err := t.GetMetadataContext(userContext, request)
+	if err != nil {
+		return err
+	}
+	_, err = t.client.AddFramework(md, request)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (t *TrustRegistryBase) RegisterIssuer(userContext context.Context, request *sdk.RegisterIssuerRequest) error {
+	md, err := t.GetMetadataContext(userContext, request)
+	if err != nil {
+		return err
+	}
+	_, err = t.client.RegisterIssuer(md, request)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (t *TrustRegistryBase) UnregisterIssuer(userContext context.Context, request *sdk.UnregisterIssuerRequest) error {
+	panic("implement me")
+}
+
+func (t *TrustRegistryBase) RegisterVerifier(userContext context.Context, request *sdk.RegisterVerifierRequest) error {
+	md, err := t.GetMetadataContext(userContext, request)
+	if err != nil {
+		return err
+	}
+	_, err = t.client.RegisterVerifier(md, request)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (t *TrustRegistryBase) UnregisterVerifier(userContext context.Context, request *sdk.UnregisterVerifierRequest) error {
+	panic("implement me")
+}
+
+func (t *TrustRegistryBase) CheckIssuerStatus(userContext context.Context, request *sdk.CheckIssuerStatusRequest) (sdk.RegistrationStatus, error) {
+	md, err := t.GetMetadataContext(userContext, request)
+	if err != nil {
+		return sdk.RegistrationStatus_NOT_FOUND, err
+	}
+	response, err := t.client.CheckIssuerStatus(md, request)
+	if err != nil {
+		return sdk.RegistrationStatus_NOT_FOUND, err
+	}
+	return response.Status, nil
+}
+
+func (t *TrustRegistryBase) CheckVerifierStatus(userContext context.Context, request *sdk.CheckVerifierStatusRequest) (sdk.RegistrationStatus, error) {
+	md, err := t.GetMetadataContext(userContext, request)
+	if err != nil {
+		return sdk.RegistrationStatus_NOT_FOUND, err
+	}
+	response, err := t.client.CheckVerifierStatus(md, request)
+	if err != nil {
+		return sdk.RegistrationStatus_NOT_FOUND, err
+	}
+	return response.Status, nil
+}
+
+func (t *TrustRegistryBase) SearchRegistry(userContext context.Context, query string) (*sdk.SearchRegistryResponse, error) {
+	request := &sdk.SearchRegistryRequest{
+		Query:   query,
+		Options: &sdk.RequestOptions{ResponseJsonFormat: sdk.JsonFormat_Protobuf},
+	}
+	md, err := t.GetMetadataContext(userContext, request)
+	if err != nil {
+		return nil, err
+	}
+	response, err := t.client.SearchRegistry(md, request)
+	if err != nil {
+		return nil, err
+	}
+	return response, nil
+}
