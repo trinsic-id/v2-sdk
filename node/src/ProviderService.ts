f1a5edf8213eec44255e2fd2764e3b0b27848180
Scott Phillips <polygonguru@gmail.com>
2021-12-27 14:26:44 -0500

    ProviderService, TrustRegistryService, WalletService updates

--- node/src/ProviderService.ts
+++ node/src/ProviderService.ts
@@ -1,35 +1,63 @@
-import ServiceBase, { ServiceOptions } from "./ServiceBase";
-import { AccountProfile, ProviderClient, ServerConfig } from "./proto";
-import { InvitationStatusRequest, InvitationStatusResponse, InviteRequest, InviteResponse } from "./proto";
+import ServiceBase, {ServiceOptions} from "./ServiceBase";
+import {
+    CreateEcosystemRequest, CreateEcosystemResponse,
+    InvitationStatusRequest,
+    InvitationStatusResponse,
+    InviteRequest,
+    InviteResponse, ListEcosystemsRequest, ListEcosystemsResponse,
+    ProviderClient
+} from "./proto";
 
 export class ProviderService extends ServiceBase {
-  client: ProviderClient;
+    client: ProviderClient;
 
-  constructor(options?: ServiceOptions) {
-    super(options);
+    constructor(options?: ServiceOptions) {
+        super(options);
 
-    this.client = new ProviderClient(this.address, this.channelCredentials);
-  }
+        this.client = new ProviderClient(this.address, this.channelCredentials);
+    }
 
-  public inviteParticipant(request: InviteRequest): Promise<InviteResponse> {
-    return new Promise(async (resolve, reject) => {
-      this.client.invite(request, await this.getMetadata(request), (error, response) => {
-        if (error) {
-          reject(error);
-        }
-        return resolve(response);
-      });
-    });
-  }
+    public inviteParticipant(request: InviteRequest): Promise<InviteResponse> {
+        return new Promise(async (resolve, reject) => {
+            this.client.invite(request, await this.getMetadata(request), (error, response) => {
+                if (error) {
+                    reject(error);
+                }
+                return resolve(response);
+            });
+        });
+    }
 
-  public invitationStatus(request: InvitationStatusRequest): Promise<InvitationStatusResponse> {
-    return new Promise(async (resolve, reject) => {
-      this.client.invitationStatus(request, await this.getMetadata(request), (error, response) => {
-        if (error) {
-          reject(error);
-        }
-        return resolve(response);
-      });
-    });
-  }
+    public invitationStatus(request: InvitationStatusRequest): Promise<InvitationStatusResponse> {
+        return new Promise(async (resolve, reject) => {
+            this.client.invitationStatus(request, await this.getMetadata(request), (error, response) => {
+                if (error) {
+                    reject(error);
+                }
+                return resolve(response);
+            });
+        });
+    }
+
+    public createEcosystem(request: CreateEcosystemRequest): Promise<CreateEcosystemResponse> {
+        return new Promise(async (resolve, reject) => {
+            this.client.createEcosystem(request, await this.getMetadata(request), (error, response) => {
+                if (error) {
+                    reject(error);
+                }
+                return resolve(response);
+            });
+        });
+    }
+
+    public listEcosystems(request: ListEcosystemsRequest): Promise<ListEcosystemsResponse> {
+        return new Promise(async (resolve, reject) => {
+            this.client.listEcosystems(request, await this.getMetadata(request), (error, response) => {
+                if (error) {
+                    reject(error);
+                }
+                return resolve(response);
+            });
+        });
+    }
 }
