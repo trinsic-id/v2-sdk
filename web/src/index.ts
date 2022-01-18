2e796b0cd75ae6cb27672faa63d2cc16906a310d
Scott Phillips <polygonguru@gmail.com>
2022-01-17 21:32:54 -0500

    fix node web module import error (#380)

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,15 +1,12 @@
-import { WalletService } from './WalletService';
-import { ProviderService } from './ProviderService';
-import { TrustRegistryService } from './TrustRegistryService';
-import { CredentialService } from './CredentialService';
-import { AccountService } from './AccountService';
+import {WalletService} from './WalletService';
+import {ProviderService} from './ProviderService';
+import {TrustRegistryService} from './TrustRegistryService';
+import {CredentialService} from './CredentialService';
+import {TemplateService} from './TemplateService';
+import {AccountService} from './AccountService';
 
 export * from "./proto/index";
 
 export {
-    WalletService,
-    ProviderService,
-    TrustRegistryService,
-    AccountService,
-    CredentialService
+    WalletService, ProviderService, TrustRegistryService, AccountService, CredentialService, TemplateService
 }
