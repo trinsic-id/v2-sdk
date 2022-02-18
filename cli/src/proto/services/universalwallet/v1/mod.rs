b99871e7bb16ea1c7ad2dfc7a78758d21423f8af
Tomislav Markovski <tomislav@trinsic.id>
2022-02-18 07:39:48 -0700

    Update generated files for CLI

--- cli/src/proto/services/universalwallet/v1/mod.rs
+++ cli/src/proto/services/universalwallet/v1/mod.rs
@@ -43,9 +43,16 @@ pub struct InsertItemResponse {
 }
 /// Delete item request
 #[derive(Clone, PartialEq, ::prost::Message)]
-pub struct DeleteItemRequest {}
+pub struct DeleteItemRequest {
+    /// item identifier of the record to delete
+    #[prost(string, tag = "1")]
+    pub item_id: ::prost::alloc::string::String,
+}
 #[derive(Clone, PartialEq, ::prost::Message)]
-pub struct DeleteItemResponse {}
+pub struct DeleteItemResponse {
+    #[prost(enumeration = "super::super::common::v1::ResponseStatus", tag = "1")]
+    pub status: i32,
+}
 #[doc = r" Generated client implementations."]
 pub mod universal_wallet_client {
     #![allow(unused_variables, dead_code, missing_docs, clippy::let_unit_value)]
@@ -141,7 +148,7 @@ pub mod universal_wallet_client {
             self.inner.unary(request.into_request(), path, codec).await
         }
         #[doc = " Delete an item from the wallet permanently"]
-        pub async fn deleteitem(
+        pub async fn delete_item(
             &mut self,
             request: impl tonic::IntoRequest<super::DeleteItemRequest>,
         ) -> Result<tonic::Response<super::DeleteItemResponse>, tonic::Status> {
@@ -153,7 +160,7 @@ pub mod universal_wallet_client {
             })?;
             let codec = tonic::codec::ProstCodec::default();
             let path = http::uri::PathAndQuery::from_static(
-                "/services.universalwallet.v1.UniversalWallet/Deleteitem",
+                "/services.universalwallet.v1.UniversalWallet/DeleteItem",
             );
             self.inner.unary(request.into_request(), path, codec).await
         }
