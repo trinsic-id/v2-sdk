9515bf21769e7af91d84a6ae258b801dd22814d1
Scott Phillips <polygonguru@gmail.com>
2022-03-24 08:15:05 -0400

    rubocop fixes

--- ruby/lib/security.rb
+++ ruby/lib/security.rb
@@ -9,7 +9,7 @@ module Trinsic
   # Interface
   module ISecurityProvider
     def get_auth_header(account_profile, message)
-      raise "Not implemented"
+      raise 'Not implemented'
     end
   end
 
@@ -17,20 +17,20 @@ module Trinsic
     include ISecurityProvider
 
     def base64_binary_encode(message)
-      Base64.urlsafe_encode64(Google::Protobuf::encode(message))
+      Base64.urlsafe_encode64(Google::Protobuf.encode(message))
     end
 
     def get_auth_header(account_profile, message)
       if account_profile.protection.enabled
-        raise "The token must be unprotected before use"
+        raise 'The token must be unprotected before use'
       end
 
-      request_hash = Google::Protobuf::encode(message)
+      request_hash = Google::Protobuf.encode(message)
       request_hash = Okapi::Hashing.blake3_hash(Okapi::Hashing::V1::Blake3HashRequest.new(data: request_hash)).digest unless request_hash.length == 0 # skip hashing if empty
-      nonce = Trinsic::Common_V1::Nonce.new(timestamp: (Time.now.to_f*1000).to_int, request_hash: request_hash)
+      nonce = Trinsic::Common_V1::Nonce.new(timestamp: (Time.now.to_f * 1000).to_int, request_hash: request_hash)
       request = Okapi::Security::V1::CreateOberonProofRequest.new(token: account_profile.auth_token,
                                                                   data: account_profile.auth_data,
-                                                                  nonce: Google::Protobuf::encode(nonce))
+                                                                  nonce: Google::Protobuf.encode(nonce))
       proof = Okapi::Oberon.create_proof(request)
 
       # Convert to base-64
