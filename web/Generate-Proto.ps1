41f8c7f0b6ca5d9b13dc943f4ff5e7a9007a0cb6
Scott Phillips <polygonguru@gmail.com>
2022-03-30 13:30:15 -0400

    web side fixes

--- web/Generate-Proto.ps1
+++ web/Generate-Proto.ps1
@@ -2,6 +2,7 @@
 param($OutDir = "$PSScriptRoot/src/proto")
 
 $PROTOC_GEN_TS_PATH = Resolve-Path "$PSScriptRoot/node_modules/.bin/protoc-gen-ts$(If ($IsWindows) {'.cmd'} Else {''})"
+$PROTOC_GEN_GRPC_PATH = Resolve-Path "$PSScriptRoot/node_modules/.bin/protoc-gen-grpc-web$(If ($IsWindows) {'.cmd'} Else {''})"
 # For mac, `brew install protobuf`
 # for windows, download and install to PATH
 # for linux, `apt-get install protobuf`
@@ -18,5 +19,7 @@ if ((Get-Command "$PROTOC" -ErrorAction SilentlyContinue) -eq $null)
 foreach ($Item in Get-ChildItem -Path $PROTO_DIR -Include *.proto -Recurse)
 {
     $File = $Item.FullName
-    &"$PROTOC" --plugin=import_style=typescript:protoc-gen-grpc-web=$PROTOC_GEN_TS_PATH --js_out=import_style=commonjs,binary:$OUTPUT_DIR --grpc-web_out=import_style=typescript,mode=grpcwebtext:$OUTPUT_DIR -I $PROTO_DIR $File
+    $Expr = "$PROTOC --plugin=import_style=typescript:protoc-gen-grpc-web=$PROTOC_GEN_GRPC_PATH --plugin=protoc-gen-ts=$PROTOC_GEN_TS_PATH --js_out=import_style=commonjs,binary:$OUTPUT_DIR --grpc-web_out=import_style=typescript,mode=grpcwebtext:$OUTPUT_DIR -I $PROTO_DIR $File"
+    Write-Output $Expr
+    Invoke-Expression $Expr
 }
