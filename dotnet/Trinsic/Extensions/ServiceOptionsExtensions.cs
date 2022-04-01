64ca22afdd136e0df0bf62b909ea9adfa3984c93
Scott Phillips <polygonguru@gmail.com>
2022-04-01 14:18:52 -0400

    move `CloneWithAuthToken()` extension method

--- dotnet/Trinsic/Extensions/ServiceOptionsExtensions.cs
+++ dotnet/Trinsic/Extensions/ServiceOptionsExtensions.cs
@@ -7,4 +7,10 @@ public static class ServiceOptionsExtensions
     public static string FormatUrl(this ServiceOptions options) {
         return $"{(options.ServerUseTls ? "https" : "http")}://{options.ServerEndpoint}:{options.ServerPort}";
     }
+
+    public static ServiceOptions CloneWithAuthToken(this ServiceOptions options, string authToken) {
+        var cloned = options.Clone();
+        cloned.AuthToken = authToken;
+        return cloned;
+    }
 }
