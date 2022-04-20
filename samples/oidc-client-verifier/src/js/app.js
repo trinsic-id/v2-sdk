62cb76dc0cd4ea1ff32617b5d24b0c61706e974c
Joshua Coffey <>
2022-04-20 16:06:02 +0000

    Work on OIDC sample visual design

--- samples/oidc-client-verifier/src/js/app.js
+++ samples/oidc-client-verifier/src/js/app.js
@@ -4,14 +4,30 @@
 const TRINSIC_ECOSYSTEM = "GeraldsGyms";
 
 //Schema URL(s) of credentials we would like to retrieve from the user.
-const TRINSIC_SCHEMAS = ["https://dev-schema.trinsic.cloud/GeraldsGyms/gym-membership"];
+const TRINSIC_SCHEMAS = ["https://schema.trinsic.cloud/GeraldsGyms/gym-membership"];
+
+/***************************************************
+ *    Probably don't modify anything below here!   *
+ ***************************************************/
+
+//Trinsic deployment environment to run against (prod, staging, dev)
+const TRINSIC_ENVIRONMENT = "prod";
+
+//connect, connect-dev, connect-staging
+let authority_subdomain = "connect";
+if(TRINSIC_ENVIRONMENT !== "prod") {
+  authority_subdomain += "-" + TRINSIC_ENVIRONMENT;
+}
+
+let authority = "https://" + authority_subdomain + ".trinsic.cloud/";
 
 
 let config = {
-    authority: "https://localhost:7266/", //TODO: replace with live OIDC endpoint
+    authority: "https://connect-dev.trinsic.cloud/",
+    // authority: "https://localhost:7266/",
     client_id: "urn:walletId:1", //TODO: discuss client IDs
     redirect_uri: window.location.origin + "/callback.html",
-    post_logout_redirect_uri: window.location.origin + "/index.html",
+    post_logout_redirect_uri: window.location.origin + "/index.html", 
 
     response_type: "code",
     scope: "openid",
