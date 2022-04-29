15c286dca06c7bf7a20a8ab575533ed77aa3ab26
Scott Phillips <polygonguru@gmail.com>
2022-04-29 09:23:55 -0400

    Remove node, since it is now part of the web package

--- node/test/env.ts
+++
@@ -1,4 +0,0 @@
-import { ServiceOptions } from "../src"
-
-export const options = ServiceOptions.fromPartial({serverEndpoint: "staging-internal.trinsic.cloud",
-    serverPort: 443, serverUseTls: true})
