4985197bf8d23ec74414acb39ff3bdd572e09553
Joshua Coffey <josh@trinsic.id>
2022-05-23 16:45:25 -0400

    Vaccine walkthrough improvements (#664)

--- web/test/VaccineDemo.spec.ts
+++ web/test/VaccineDemo.spec.ts
@@ -8,7 +8,7 @@ import {set20SecTimeout} from "./env";
 describe("Demo: vaccination demo - credential issuance, storing, and verification", () => {
   set20SecTimeout()
   it("should run the demo without raising exceptions", async () => {
-    let response = await vaccineDemo(vaccineCertUnsigned, vaccineCertFrame);
+    let response = await vaccineDemo();
     expect(response.isValid).toBeTruthy();
   });
 });
