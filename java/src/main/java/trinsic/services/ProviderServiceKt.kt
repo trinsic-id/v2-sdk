30c39f56335c08eeedbaede1eff0fb0f8391331f
Scott Phillips <polygonguru@gmail.com>
2022-06-09 22:12:28 -0400

    Java & Kotlin 615, 666, 719

--- java/src/main/java/trinsic/services/ProviderServiceKt.kt
+++ java/src/main/java/trinsic/services/ProviderServiceKt.kt
@@ -9,6 +9,36 @@ import trinsic.services.common.v1.ProviderOuterClass.*
 class ProviderServiceKt(options: Options.ServiceOptions?) : ServiceBase(options) {
   var stub = ProviderGrpcKt.ProviderCoroutineStub(this.channel)
 
+    @Throws(InvalidProtocolBufferException::class, DidException::class)
+    suspend fun createEcosystem(request: CreateEcosystemRequest): CreateEcosystemResponse {
+        return stub.createEcosystem(request)
+    }
+
+    @Throws(InvalidProtocolBufferException::class, DidException::class)
+    suspend fun updateEcosystem(request: UpdateEcosystemRequest): UpdateEcosystemResponse {
+        return stub.updateEcosystem(request)
+    }
+
+    @Throws(InvalidProtocolBufferException::class, DidException::class)
+    suspend fun addWebhook(request: AddWebhookRequest): AddWebhookResponse {
+        return stub.addWebhook(request)
+    }
+
+    @Throws(InvalidProtocolBufferException::class, DidException::class)
+    suspend fun deleteWebhook(request: DeleteWebhookRequest): DeleteWebhookResponse {
+        return stub.deleteWebhook(request)
+    }
+
+    @Throws(InvalidProtocolBufferException::class, DidException::class)
+    suspend fun ecosystemInfo(request: EcosystemInfoRequest): EcosystemInfoResponse {
+        return stub.ecosystemInfo(request)
+    }
+
+    @Throws(InvalidProtocolBufferException::class, DidException::class)
+    suspend fun generateToken(request: GenerateTokenRequest): GenerateTokenResponse {
+        return stub.generateToken(request)
+    }
+
   @Throws(InvalidProtocolBufferException::class, DidException::class)
   suspend fun inviteParticipant(request: InviteRequest): InviteResponse {
     return withMetadata(stub, request).invite(request)
@@ -20,8 +50,13 @@ class ProviderServiceKt(options: Options.ServiceOptions?) : ServiceBase(options)
     return withMetadata(stub, request).invitationStatus(request)
   }
 
-  @Throws(InvalidProtocolBufferException::class, DidException::class)
-  suspend fun createEcosystem(request: CreateEcosystemRequest): CreateEcosystemResponse {
-    return stub.createEcosystem(request)
-  }
+    @Throws(InvalidProtocolBufferException::class, DidException::class)
+    suspend fun getOberonKey(request: GetOberonKeyRequest): GetOberonKeyResponse {
+        return stub.getOberonKey(request)
+    }
+
+    @Throws(InvalidProtocolBufferException::class, DidException::class)
+    suspend fun getEventToken(request: GetEventTokenRequest): GetEventTokenResponse {
+        return stub.getEventToken(request)
+    }
 }
