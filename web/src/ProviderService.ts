0dd0072ea71ca018026a1164829163f3e0f1d790
Scott Phillips <polygonguru@gmail.com>
2022-06-09 20:08:25 -0400

    Typescript for 615, 666, 719 (#733)

--- web/src/ProviderService.ts
+++ web/src/ProviderService.ts
@@ -1,13 +1,24 @@
 import ServiceBase from "./ServiceBase";
 import {
-  CreateEcosystemRequest,
-  CreateEcosystemResponse,
-  InvitationStatusRequest,
-  InvitationStatusResponse,
-  InviteRequest,
-  InviteResponse,
-  ProviderDefinition,
-  ServiceOptions,
+    AddWebhookRequest, AddWebhookResponse,
+    CreateEcosystemRequest,
+    CreateEcosystemResponse, DeleteWebhookRequest, DeleteWebhookResponse,
+    EcosystemInfoRequest,
+    EcosystemInfoResponse,
+    GenerateTokenRequest,
+    GenerateTokenResponse,
+    GetEventTokenRequest,
+    GetEventTokenResponse,
+    GetOberonKeyRequest,
+    GetOberonKeyResponse,
+    InvitationStatusRequest,
+    InvitationStatusResponse,
+    InviteRequest,
+    InviteResponse,
+    ProviderDefinition,
+    ServiceOptions,
+    UpdateEcosystemRequest,
+    UpdateEcosystemResponse,
 } from "./proto";
 
 import type { Client as BrowserClient } from "nice-grpc-web";
@@ -19,6 +30,62 @@ export class ProviderService extends ServiceBase {
     super(options);
 
     this.client = this.createClient(ProviderDefinition);
+  }
+
+  public async createEcosystem(
+    request: CreateEcosystemRequest
+  ): Promise<CreateEcosystemResponse> {
+    return this.client.createEcosystem(request);
+  }
+
+  public async updateEcosystem(
+    request: UpdateEcosystemRequest
+  ): Promise<UpdateEcosystemResponse> {
+    return this.client.updateEcosystem(request, {
+      metadata: await this.getMetadata(
+        UpdateEcosystemRequest.encode(request).finish()
+      ),
+    });
+  }
+
+  public async addWebhook(
+    request: AddWebhookRequest
+  ): Promise<AddWebhookResponse> {
+    return this.client.addWebhook(request, {
+      metadata: await this.getMetadata(
+        AddWebhookRequest.encode(request).finish()
+      ),
+    });
+  }
+
+  public async deleteWebhook(
+    request: DeleteWebhookRequest
+  ): Promise<DeleteWebhookResponse> {
+    return this.client.deleteWebhook(request, {
+      metadata: await this.getMetadata(
+          DeleteWebhookRequest.encode(request).finish()
+      ),
+    });
+  }
+
+  public async ecosystemInfo(
+    request: EcosystemInfoRequest
+  ): Promise<EcosystemInfoResponse> {
+    return this.client.ecosystemInfo(request, {
+      metadata: await this.getMetadata(
+        EcosystemInfoRequest.encode(request).finish()
+      ),
+    });
+  }
+
+  public async generateToken(
+    request: GenerateTokenRequest
+  ): Promise<GenerateTokenResponse> {
+    return this.client.generateToken(request, {
+      metadata: await this.getMetadata(
+        GenerateTokenRequest.encode(request).finish()
+      ),
+    });
   }
 
   public async inviteParticipant(
@@ -39,9 +106,23 @@ export class ProviderService extends ServiceBase {
     });
   }
 
-  public async createEcosystem(
-    request: CreateEcosystemRequest
-  ): Promise<CreateEcosystemResponse> {
-    return this.client.createEcosystem(request);
+  public async getOberonKey(
+    request: GetOberonKeyRequest
+  ): Promise<GetOberonKeyResponse> {
+    return this.client.getOberonKey(request, {
+      metadata: await this.getMetadata(
+        GetOberonKeyRequest.encode(request).finish()
+      ),
+    });
+  }
+
+  public async getEventToken(
+    request: GetEventTokenRequest
+  ): Promise<GetEventTokenResponse> {
+    return this.client.getEventToken(request, {
+      metadata: await this.getMetadata(
+        GetEventTokenRequest.encode(request).finish()
+      ),
+    });
   }
 }
---
+++ web/src/TrinsicService.ts
@@ -0,0 +1,64 @@
+import ServiceBase from "./ServiceBase";
+import {ServiceOptions} from "./proto/sdk/options/v1/options";
+import {AccountService} from "./AccountService";
+import {CredentialService} from "./CredentialService";
+import {TemplateService} from "./TemplateService";
+import {ProviderService} from "./ProviderService";
+import {TrustRegistryService} from "./TrustRegistryService";
+import {WalletService} from "./WalletService";
+
+
+export class TrinsicService extends ServiceBase {
+  private _account: AccountService | undefined;
+  private _credential: CredentialService | undefined;
+  private _provider: ProviderService | undefined;
+  private _template: TemplateService | undefined;
+  private _trustRegistry: TrustRegistryService | undefined;
+  private _wallet: WalletService | undefined;
+
+  constructor(options?: ServiceOptions) {
+    super(options);
+  }
+
+  public account(): AccountService {
+      this._account =
+        this._account || new AccountService(this.options);
+
+      return this._account!;
+  }
+
+    public credential(): CredentialService {
+        this._credential =
+            this._credential || new CredentialService(this.options);
+
+        return this._credential!;
+    }
+
+    public provider(): ProviderService {
+        this._provider =
+            this._provider || new ProviderService(this.options);
+
+        return this._provider!;
+    }
+
+    public template(): TemplateService {
+        this._template =
+            this._template || new TemplateService(this.options);
+
+        return this._template!;
+    }
+
+    public trustRegistry(): TrustRegistryService {
+        this._trustRegistry =
+            this._trustRegistry || new TrustRegistryService(this.options);
+
+        return this._trustRegistry!;
+    }
+
+    public wallet(): WalletService {
+        this._wallet =
+            this._wallet || new WalletService(this.options);
+
+        return this._wallet!;
+    }
+}
