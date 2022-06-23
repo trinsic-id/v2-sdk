25794cbe8e428aa0e8d03e4504c07bedd52fe5d9
Joshua Coffey <josh@trinsic.id>
2022-06-23 09:51:47 -0400

    Add all CLI commands from #769 (#784)
    * Add all CLI commands from #769
    
    * Remove `req2` and don't output anything in `authorize_webhook`

--- cli/src/services/provider.rs
+++ cli/src/services/provider.rs
@@ -1,5 +1,6 @@
 use super::Output;
 use super::{super::parser::provider::*, Item};
+use crate::proto::services::provider::v1::{AddWebhookRequest, DeleteWebhookRequest, EcosystemInfoRequest, UpdateEcosystemRequest};
 use crate::utils::to_value;
 use crate::{
     dict,
@@ -20,6 +21,10 @@ pub(crate) fn execute(args: &Command, config: CliConfig) -> Result<Output, Error
     match args {
         Command::Invite(args) => invite(args, &config),
         Command::CreateEcosystem(args) => create_ecosystem(args, &config),
+        Command::UpdateEcosystem(args) => update_ecosystem(args, &config),
+        Command::EcosystemInfo => ecosystem_info(&config),
+        Command::AddWebhook(args) => add_webhook(args, &config),
+        Command::DeleteWebhook(args) => delete_webhook(args, &config),
         Command::InvitationStatus => todo!(),
     }
 }
@@ -72,5 +77,73 @@ async fn create_ecosystem(args: &CreateEcosystemArgs, config: &CliConfig) -> Res
         "ecosystem".into() => Item::Json(to_value(&response.ecosystem
             .ok_or(Error::InvalidArgument("expected ecosystem object in response".to_string()))?)?),
         "auth token".into() => Item::String(auth_token)
+    })
+}
+
+#[tokio::main]
+async fn update_ecosystem(args: &UpdateEcosystemArgs, config: &CliConfig) -> Result<Output, Error> {
+    let mut client = grpc_client_with_auth!(ProviderClient<Channel>, config.to_owned());
+
+    let request = tonic::Request::new(UpdateEcosystemRequest {
+        description: args.description.as_ref().map_or(String::default(), |x| x.to_owned()),
+        uri: args.uri.as_ref().map_or(String::default(), |x| x.to_owned()),
+    });
+
+    let response = client.update_ecosystem(request).await?.into_inner();
+
+    Ok(dict! {
+        "ecosystem".into() => Item::Json(to_value(
+                &response.ecosystem.ok_or(Error::InvalidArgument("expected ecosystem object in response".to_string()))?
+        )?)
+    })
+}
+
+#[tokio::main]
+async fn ecosystem_info(config: &CliConfig) -> Result<Output, Error> {
+    let mut client = grpc_client_with_auth!(ProviderClient<Channel>, config.to_owned());
+
+    let request = tonic::Request::new(EcosystemInfoRequest {});
+    let response = client.ecosystem_info(request).await?.into_inner();
+
+    Ok(dict! {
+        "ecosystem".into() => Item::Json(to_value(
+                &response.ecosystem.ok_or(Error::InvalidArgument("expected ecosystem object in response".to_string()))?
+        )?)
+    })
+}
+
+#[tokio::main]
+async fn add_webhook(args: &AddWebhookArgs, config: &CliConfig) -> Result<Output, Error> {
+    let mut client = grpc_client_with_auth!(ProviderClient<Channel>, config.to_owned());
+
+    let request = tonic::Request::new(AddWebhookRequest {
+        events: args.events.clone(),
+        destination_url: args.url.clone(),
+        secret: args.secret.clone(),
+    });
+
+    let response = client.add_webhook(request).await?.into_inner();
+
+    Ok(dict! {
+        "ecosystem".into() => Item::Json(to_value(
+                &response.ecosystem.ok_or(Error::InvalidArgument("expected ecosystem object in response".to_string()))?
+        )?)
+    })
+}
+
+#[tokio::main]
+async fn delete_webhook(args: &DeleteWebhookArgs, config: &CliConfig) -> Result<Output, Error> {
+    let mut client = grpc_client_with_auth!(ProviderClient<Channel>, config.to_owned());
+
+    let request = tonic::Request::new(DeleteWebhookRequest {
+        webhook_id: args.webhook_id.clone(),
+    });
+
+    let response = client.delete_webhook(request).await?.into_inner();
+
+    Ok(dict! {
+        "ecosystem".into() => Item::Json(to_value(
+                &response.ecosystem.ok_or(Error::InvalidArgument("expected ecosystem object in response".to_string()))?
+        )?)
     })
 }
