d61232cf6d2b21f7c8c26cac5d5853edbb6a893c
Joshua Coffey <josh@trinsic.id>
2022-06-29 12:00:30 -0400

    Fix duplicate export (#809)

--- web/src/index.ts
+++ web/src/index.ts
@@ -6,7 +6,6 @@ import { TemplateService } from "./TemplateService";
 import { AccountService } from "./AccountService";
 import { TrinsicService } from "./TrinsicService";
 import ServiceBase from "./ServiceBase";
-import {TrinsicService} from "./TrinsicService";
 
 export * from "./proto/index";
 
@@ -18,6 +17,5 @@ export {
   AccountService,
   CredentialService,
   TemplateService,
-    TrinsicService,
   ServiceBase,
 };
