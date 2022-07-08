01b7a4bd9fcb190a047984a49bfb8174423ff348
Joshua Coffey <josh@trinsic.id>
2022-07-08 12:40:39 -0400

    Dotnet metadata changes

--- dotnet/Trinsic/ServiceBase.cs
+++ dotnet/Trinsic/ServiceBase.cs
@@ -1,11 +1,13 @@
 using System;
 using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
 using Grpc.Core;
 using Google.Protobuf;
 using Trinsic.Services.Common.V1;
 using Trinsic.Services.Account.V1;
 using System.Threading.Tasks;
 using Grpc.Net.Client;
+using Okapi.Metadata;
 using Trinsic.Sdk.Options.V1;
 #if __BROWSER__
 using System.Net.Http;
@@ -71,46 +73,92 @@ public abstract class ServiceBase
     protected GrpcChannel Channel { get; }
 
     /// <summary>
-    /// Create call metadata by setting the required authentication headers
+    /// The cached metadata of the Okapi library being used.
+    ///
+    /// Do not read this variable directly -- use `GetOkapiMetadata()`.
     /// </summary>
+    private MetadataResponse? CachedOkapiMetadata { get; set; } = null;
+
+    /// <summary>
+    /// Create call metadata by setting authentication and version headers
+    ///
+    /// TODO: `request` only has relevance if `authenticated` == true. Decide what to do about that.
+    /// </summary>
+    /// <param name="request">Protobuf request message to create headers for</param>
+    /// <param name="authenticated">If true, authentication headers will be built for request -- and exception will be thrown if no authentication token is available to do so</param>
     /// <returns></returns>
-    protected async Task<Metadata> BuildMetadataAsync(IMessage request) {
+    protected async Task<Metadata> BuildMetadataAsync(IMessage request, bool authenticated = true) {
+        var headers = new Metadata() {
+            {"TrinsicSDKLanguage", "dotnet"},
+            {"TrinsicSDKVersion", GetSdkVersion()},
+            {"TrinsicOkapiVersion", GetOkapiMetadata().Version}
+        };
+
+        // If no authentication needed, return early
+        if (!authenticated)
+            return headers;
+
+        // Build Authentication header
         var authToken = string.IsNullOrWhiteSpace(Options.AuthToken)
             ? await TokenProvider.GetAsync()
             : Options.AuthToken;
 
-        // Return empty metadata if no auth token
         if (authToken is null)
-        {
-            return new();
-        }
+            throw new("Cannot call authenticated endpoint before signing in");
 
         var profile = AccountProfile.Parser.ParseFrom(Base64Url.DecodeBytes(authToken));
 
-        return new() {
-            { "Authorization", await _securityProvider.GetAuthHeaderAsync(profile, request) }
-        };
+        headers.Add("Authorization", await _securityProvider.GetAuthHeaderAsync(profile, request));
+        return headers;
     }
 
     /// <summary>
-    /// Create call metadata by setting the required authentication headers
+    /// Create call metadata by setting the required authentication and version headers
+    ///
+    /// TODO: `request` only has relevance if `authenticated` == true. Decide what to do about that.
     /// </summary>
+    /// <param name="request">Protobuf request message to create headers for</param>
+    /// <param name="authenticated">If true, authentication headers will be built for request -- and exception will be thrown if no authentication token is available to do so</param>
     /// <returns></returns>
-    protected Metadata BuildMetadata(IMessage request) {
+    protected Metadata BuildMetadata(IMessage request, bool authenticated = true) {
+        var headers = new Metadata() {
+            {"TrinsicSDKLanguage", "dotnet"},
+            {"TrinsicSDKVersion", GetSdkVersion()},
+            {"TrinsicOkapiVersion", GetOkapiMetadata().Version}
+        };
+
+        // If no authentication needed, return early
+        if (!authenticated)
+            return headers;
+
+        // Build authentication header
         var authToken = string.IsNullOrWhiteSpace(Options.AuthToken)
             ? TokenProvider.Get()
             : Options.AuthToken;
-
-        // Return empty metadata if no auth token
+        
         if (authToken is null)
-        {
-            return new();
-        }
+            throw new("Cannot call authenticated endpoint before signing in");
 
         var profile = AccountProfile.Parser.ParseFrom(Base64Url.DecodeBytes(authToken));
 
-        return new() {
-            { "Authorization", _securityProvider.GetAuthHeader(profile, request) }
-        };
+        headers.Add("Authorization", _securityProvider.GetAuthHeader(profile, request));
+        return headers;
+    }
+
+    /// <summary>
+    /// Fetches the current version of the SDK
+    /// </summary>
+    /// <returns></returns>
+    private string GetSdkVersion() {
+        // TODO: this is always returning `1.0.0.0` because that's what's specified in the project settings. Figure out how to get the actual version number.
+        return Assembly.GetAssembly(typeof(ServiceBase))?.GetName().Version?.ToString() ?? "unknown";
+    }
+
+    /// <summary>
+    /// Returns metadata from the Okapi library, caching in-memory to avoid repeat requests
+    /// </summary>
+    /// <returns></returns>
+    private MetadataResponse GetOkapiMetadata() {
+        return CachedOkapiMetadata ??= OkapiMetadata.GetMetadata();
     }
 }
