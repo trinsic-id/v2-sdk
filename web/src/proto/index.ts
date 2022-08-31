fb7736feebde2f6944c22f801c4f29818dbafe83
Scott Phillips <polygonguru@gmail.com>
2022-08-31 13:37:28 -0400

    missing debug_service implementations

--- web/src/proto/index.ts
+++ web/src/proto/index.ts
@@ -1,6 +1,7 @@
 export * from "./sdk/options/v1/options";
 export * from "./services/common/v1/common";
 export * from "./services/account/v1/account";
+export * from "./services/debug/v1/debug";
 export * from "./services/verifiable-credentials/v1/verifiable-credentials";
 export * from "./services/verifiable-credentials/templates/v1/templates";
 export * from "./services/provider/v1/provider";
