3d8c04a7c35f23cd7096b531951fbe3cd14295df
Scott Phillips <polygonguru@gmail.com>
2022-09-20 13:29:06 -0400

    make oidc demo run against dev instead of local (#1071)

--- samples/oidc-client-verifier/src/setupDemo.ts
+++ samples/oidc-client-verifier/src/setupDemo.ts
@@ -1,25 +1,31 @@
 import {
     AccountDetails,
-    AccountProfile, AccountService,
+    AccountService,
     CreateCredentialTemplateRequest,
     CreateCredentialTemplateResponse,
     CreateEcosystemRequest,
-    CreateEcosystemResponse, EcosystemDisplay, EcosystemDisplayDetails,
+    CreateEcosystemResponse,
     FieldType,
     InviteRequest,
     IssueFromTemplateResponse,
-    LoginRequest,
     ParticipantType,
     ServiceOptions,
     TemplateField,
-    TrinsicService, UpdateEcosystemRequest,
+    TrinsicService,
+    UpdateEcosystemRequest,
 } from "@trinsic/trinsic";
-import {
-    ECOSYSTEM_DOMAIN,
-    TRINSIC_ECOSYSTEM,
-    TRINSIC_ENVIRONMENT,
-} from "./ts/constants";
-import * as readline from "readline";
+import { ECOSYSTEM_DOMAIN, TRINSIC_ENVIRONMENT } from "./ts/constants";
+
+/**
+ * Replace these with your own values
+ */
+// const ECOSYSTEM_ID = "urn:trinsic:ecosystems:clever-bassi-N4VYHjUg2Jk";
+const ECOSYSTEM_TOKEN =
+    "CiVodHRwczovL3RyaW5zaWMuaWQvc2VjdXJpdHkvdjEvb2Jlcm9uEoMBCip1cm46dHJpbnNpYzp3YWxsZXRzOlBhUE1aQVVrTGhoS3REOWV4UkRMOUISJDc4NDViMjAyLTQ2NjQtNDAzNi1hNDhhLTlmMjRmZjBiY2RiMSIvdXJuOnRyaW5zaWM6ZWNvc3lzdGVtczpjbGV2ZXItYmFzc2ktTjRWWUhqVWcySmsaMI9-GilmVQj9QTsgTkU-tF-zT5E7LNSy_SCs66_UVptFlp8D5BC_gZmtH8Zec3paZyIA";
+const LOGO_URL_DARK = "https://cdn.trinsic.id/assets/logo/512x512.png";
+const LOGO_URL_LIGHT = "https://cdn.trinsic.id/assets/logo/512x512-circle.png";
+const COLOR_LIGHT = "#6af06f";
+const COLOR_DARK = "#601b46";
 
 const endpoints = {
     prod: "prod.trinsic.cloud",
@@ -32,33 +38,34 @@ const service = new TrinsicService(
     ServiceOptions.fromPartial({ serverEndpoint: TRINSIC_ENDPOINT })
 );
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
-
 async function verifyDomain(): Promise<boolean> {
-    let verifyRecord = await service.provider().retrieveDomainVerificationRecord({});
+    let verifyRecord = await service
+        .provider()
+        .retrieveDomainVerificationRecord({});
     console.log("verify record:", verifyRecord);
-    let verifyResult = await service.provider().refreshDomainVerificationStatus({});
+    let verifyResult = await service
+        .provider()
+        .refreshDomainVerificationStatus({});
     console.log("verify status:", verifyResult);
     return verifyResult.domainVerified;
 }
 
 async function updateEcosystem() {
-    await service.provider().updateEcosystem(UpdateEcosystemRequest.fromPartial({
-        domain: "mewmba.dev",
-        display: {
-            dark: {
-                color: "#601b46",
-                logoUrl: "https://cdn.trinsic.id/assets/logo/512x512.png"
+    await service.provider().updateEcosystem(
+        UpdateEcosystemRequest.fromPartial({
+            domain: ECOSYSTEM_DOMAIN,
+            display: {
+                dark: {
+                    color: COLOR_DARK,
+                    logoUrl: LOGO_URL_DARK,
+                },
+                light: {
+                    color: COLOR_LIGHT,
+                    logoUrl: LOGO_URL_LIGHT,
+                },
             },
-            light: {
-                color: "#6af06f",
-                logoUrl: "https://cdn.trinsic.id/assets/logo/512x512-circle.png"
-            }
-        }
-    }));
+        })
+    );
 
     let info = await service.provider().ecosystemInfo({});
     console.log("ecosystem info:", info);
@@ -66,13 +73,15 @@ async function updateEcosystem() {
 }
 
 async function createEcosystem(): Promise<CreateEcosystemResponse> {
-    let createEcosystemResponse = await service.provider().createEcosystem(
-        CreateEcosystemRequest.fromPartial({})
+    let createEcosystemResponse = await service
+        .provider()
+        .createEcosystem(CreateEcosystemRequest.fromPartial({}));
+    console.log("ecosystem id:", createEcosystemResponse.ecosystem!.id);
+    let profileToken = AccountService.convertToToken(
+        createEcosystemResponse.profile!
     );
-    console.log("ecosystem id:", createEcosystemResponse.ecosystem!.id)
-    let profileToken = AccountService.convertToToken(createEcosystemResponse.profile!);
     service.setAuthToken(profileToken);
-    console.log("ecosystem token=", profileToken)
+    console.log("ecosystem token=", profileToken);
     await updateEcosystem();
     return createEcosystemResponse;
 }
@@ -115,26 +124,22 @@ async function issueCredentialToWallet(
 }
 
 async function main(): Promise<void> {
-    let ecosystemId = "";
-    let ecosystemToken = "";
-    if (ecosystemId === "") {
-        let createEcoResp = await createEcosystem();
-    } else {
-        service.setAuthToken(ecosystemToken);
-        await updateEcosystem();
-    }
+    service.setAuthToken(ECOSYSTEM_TOKEN);
+    await updateEcosystem();
     let verifiedDomain = await verifyDomain();
     if (!verifiedDomain) {
+        console.log(
+            "Domain not verified, add the verify record as a TXT record to your domain then run again."
+        );
         return;
     }
     let createTemp = await createCoolKidsClubTemplate();
     console.log(createTemp);
-    let issueResp = await issueCredentialToWallet(createTemp.data!.id);
 }
 
-// TODO - Verify the domain (print to console the verification string)
-// TODO - Once updated, refresh verification status
-// TODO - Upload image URL and colors
+// Verify the domain (print to console the verification string)
+// Once updated, refresh verification status
+// Upload image URL and colors
 
 main().then((value) => {
     console.log("Completed!");
