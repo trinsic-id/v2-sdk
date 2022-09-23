7f6ed7c31baf36524ddeaa450befd15148c3897a
Scott Phillips <polygonguru@gmail.com>
2022-09-23 15:10:03 -0400

    Fix Dart Package Alias (#1084)

--- dart/lib/trinsic.dart
+++ dart/lib/trinsic.dart
@@ -1,13 +1,3 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
 library trinsic_dart;
-
-export 'src/account_service.dart';
-export 'src/credential_service.dart';
-export 'src/provider_service.dart';
-export 'src/template_service.dart';
-export 'src/trustregistry_service.dart';
-export 'src/wallet_service.dart';
-
-// TODO: Export any libraries intended for clients of this package.
+// Duplicate to allow for a nice export alias.
+export 'trinsic_dart.dart';
---
+++ dart/lib/trinsic_dart.dart
@@ -0,0 +1,11 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library trinsic_dart;
+
+export 'src/account_service.dart';
+export 'src/credential_service.dart';
+export 'src/provider_service.dart';
+export 'src/template_service.dart';
+export 'src/trustregistry_service.dart';
+export 'src/wallet_service.dart';
