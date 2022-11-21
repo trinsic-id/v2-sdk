e56def4cdaeadbcd0478f74512724a6ee940bdf2
chiarapaskulin <chiarapaskulin@gmail.com>
2022-11-21 13:45:50 -0300

    Update CredentialTemplateShared.ts

--- web/test/CredentialTemplateShared.ts
+++ web/test/CredentialTemplateShared.ts
@@ -7,6 +7,7 @@ import {
     InsertItemRequest,
     IssueFromTemplateRequest,
     IssueFromTemplateResponse,
+    SearchCredentialTemplatesReques,
     TemplateField,
     TrinsicService,
     VerifyProofRequest,
