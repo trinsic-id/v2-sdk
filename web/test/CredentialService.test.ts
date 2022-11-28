8e5c7ff75f7d0f3d371293361c7353be43945061
Jan-Pieter George <jp@trinsic.id>
2022-11-28 13:05:33 +0100

    tests: Add node tests for CredentialService and Templates (#1179)

---
+++ web/test/CredentialService.test.ts
@@ -0,0 +1,42 @@
+import { IssueRequest, ServiceOptions, TrinsicService } from "../node";
+// @ts-ignore
+import vaccineCertUnsigned from "./data/vaccination-certificate-unsigned.json";
+import { getTestServerOptions, setTestTimeout } from "./env";
+
+let options: ServiceOptions = getTestServerOptions();
+let trinsic: TrinsicService;
+
+describe("CredentialService Unit Tests", () => {
+    setTestTimeout();
+    beforeAll(async () => {
+        trinsic = new TrinsicService(options);
+        trinsic.options.authToken = await trinsic.account().loginAnonymous();
+    });
+
+    it("Issue Credential From Template", async () => {
+        // Get account info, so we can compare issued DID etc.
+        let info = await trinsic.account().getInfo();
+
+        // Set issuer DID of credential
+        let vaccineCert = Object.assign({}, vaccineCertUnsigned, {
+            issuer: info.publicDid,
+        });
+        let credentialJSON = JSON.stringify(vaccineCert);
+
+        // issueCredential() {
+        const issueResponse = await trinsic
+            .credential()
+            .issue(IssueRequest.fromPartial({ documentJson: credentialJSON }));
+        // }
+
+        expect(issueResponse?.signedDocumentJson).not.toBeNull();
+        const credential = JSON.parse(issueResponse?.signedDocumentJson);
+
+        expect(credential).not.toBeNull();
+        expect(credential.proof).not.toBeNull();
+        expect(credential.credentialSubject).toEqual(
+            vaccineCert.credentialSubject
+        );
+        expect(credential.issuer).toBe(info.publicDid);
+    });
+});
