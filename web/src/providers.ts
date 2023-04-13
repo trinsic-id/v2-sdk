8ed9f211549324ce71022fc21551e021a6ffa76e
Scott Phillips <polygonguru@gmail.com>
2023-04-13 11:01:56 -0400

    fix: Support node-16 for now

--- web/src/providers.ts
+++ web/src/providers.ts
@@ -4,6 +4,7 @@ import {
     createClient,
     createChannel,
     CompatServiceDefinition,
+    NodeHttpTransport, FetchTransport
 } from "nice-grpc-web";
 
 export interface IPlatformProvider {
@@ -15,13 +16,31 @@ export interface IPlatformProvider {
     ): Client<ClientService>;
 }
 
+function isNode(): boolean {
+    let isNode = false;
+    if (typeof process === 'object') {
+        if (typeof process.versions === 'object') {
+            if (typeof process.versions.node !== 'undefined') {
+                isNode = true;
+            }
+        }
+    }
+    return isNode;
+}
+
 export class BrowserProvider implements IPlatformProvider {
     createGrpcClient<ClientService extends CompatServiceDefinition>(
         definition: ClientService,
         address: string
     ): Client<ClientService> {
+        let channel: any;
+        if (isNode()) {
+            channel = createChannel(address, NodeHttpTransport())
+        } else {
+            channel = createChannel(address, FetchTransport());
+        }
         // @ts-ignore - compatible types, duplicate definitions
-        return createClient(definition, createChannel(address));
+        return createClient(definition, channel);
     }
 
     metadataLanguage(): string {
