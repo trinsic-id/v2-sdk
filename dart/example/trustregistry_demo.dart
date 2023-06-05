6e2386c5bca12beba4984b5734f6028a55180d32
Joshua Coffey <josh@trinsic.id>
2023-06-05 11:24:52 -0400

    Fix dart

--- dart/example/trustregistry_demo.dart
+++ dart/example/trustregistry_demo.dart
@@ -14,51 +14,29 @@ Future runTrustRegistryDemo() async {
   var httpsSchemaOrg = "https://schema.org/Card";
   var httpsExampleCom = "https://example.com/${uuid.v4()}";
   var didExampleTest = "did:example:test";
-  var frameworkName = "Example Framework: ${uuid.v4()}";
-
-  print("services created: $frameworkName  $httpsExampleCom");
-
-  // registerGovernanceFramework() {
-  var registerFrameworkResponse = await trinsic.trustRegistry().addFramework(
-      AddFrameworkRequest(
-          description: "Demo framework",
-          name: frameworkName,
-          governanceFrameworkUri: httpsExampleCom));
-  // }
-
-  print("Framework registered: $registerFrameworkResponse");
 
   // registerIssuerSample() {
   var registerMemberResponse = await trinsic.trustRegistry().registerMember(
       RegisterMemberRequest(
           didUri: didExampleTest,
-          frameworkId: registerFrameworkResponse.id,
           schemaUri: httpsSchemaOrg));
   // }
 
   print("Member registered: $registerMemberResponse");
 
   // checkIssuerStatus() {
-  var checkResponse = await trinsic.trustRegistry().getMembershipStatus(
-      GetMembershipStatusRequest(
+  var checkResponse = await trinsic.trustRegistry().getMemberAuthorizationStatus(
+      GetMemberAuthorizationStatusRequest(
           didUri: didExampleTest,
-          frameworkId: httpsExampleCom,
           schemaUri: httpsSchemaOrg));
   // }
 
   print("Member Status: $checkResponse");
 
   assert(checkResponse.status == RegistrationStatus.CURRENT);
-
-  // searchTrustRegistry() {
-  var searchResult = await trinsic.trustRegistry().search();
-  // }
-  assert(searchResult.itemsJson != "");
-
   // unregisterIssuer() {
   var unregisterIssuerResponse = await trinsic.trustRegistry().unregisterMember(
       UnregisterMemberRequest(
-          frameworkId: registerFrameworkResponse.id,
           schemaUri: httpsSchemaOrg,
           didUri: didExampleTest));
   // }
