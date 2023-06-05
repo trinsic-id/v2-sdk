90c8a0b7defab5677952d523c1986baba391ac50
Joshua Coffey <josh@trinsic.id>
2023-06-05 11:18:28 -0400

    Fix python

--- python/samples/trustregistry_demo.py
+++ python/samples/trustregistry_demo.py
@@ -3,9 +3,8 @@ import uuid
 
 from trinsic.proto.services.trustregistry.v1 import (
     RegistrationStatus,
-    AddFrameworkRequest,
     UnregisterMemberRequest,
-    GetMembershipStatusRequest,
+    GetMemberAuthorizationStatusRequest,
     RegisterMemberRequest,
 )
 from trinsic.proto.services.universalwallet.v1 import CreateWalletRequest
@@ -23,35 +22,21 @@ async def trustregistry_demo():
 
     # data
     https_schema_org = "https://schema.org/Card"
-    https_example_com = f"https://example.com/{uuid.uuid4()}"
     did_example_test = "did:example:test"
-    framework_name = f"Example Framework: {uuid.uuid4()}"
-
-    # registerGovernanceFramework() {
-    register_framework_response = await trinsic_service.trust_registry.add_framework(
-        request=AddFrameworkRequest(
-            governance_framework_uri=https_example_com,
-            description="Demo framework",
-            name=framework_name,
-        )
-    )
-    # }
 
     # registerIssuerSample() {
     await trinsic_service.trust_registry.register_member(
         request=RegisterMemberRequest(
             did_uri=did_example_test,
-            framework_id=register_framework_response.id,
             schema_uri=https_schema_org,
         )
     )
     # }
 
     # checkIssuerStatus() {
-    check_response = await trinsic_service.trust_registry.get_membership_status(
-        request=GetMembershipStatusRequest(
+    check_response = await trinsic_service.trust_registry.get_member_authorization_status(
+        request=GetMemberAuthorizationStatusRequest(
             did_uri=did_example_test,
-            framework_id=register_framework_response.id,
             schema_uri=https_schema_org,
         )
     )
@@ -59,17 +44,9 @@ async def trustregistry_demo():
     issuer_status = check_response.status
     assert issuer_status == RegistrationStatus.CURRENT
 
-    # searchTrustRegistry() {
-    search_result = await trinsic_service.trust_registry.search()
-    # }
-    assert search_result is not None
-    assert search_result.items_json is not None
-    assert len(search_result.items_json) > 0
-
     # unregisterIssuer() {
     unregister_issuer_response = await trinsic_service.trust_registry.unregister_member(
         request=UnregisterMemberRequest(
-            framework_id=register_framework_response.id,
             schema_uri=https_schema_org,
             did_uri=did_example_test,
         )
