ceb352bff2d57c8deef79ed2100d5655e72c9ffe
Scott Phillips <polygonguru@gmail.com>
2023-08-08 12:59:56 -0400

    fix: whatever this is?

--- samples/grpc-web/env.ts
+++ samples/grpc-web/env.ts
@@ -1,24 +1,26 @@
 import {TrinsicOptions} from "@trinsic/trinsic";
 
-export function getTestServerOptions(): TrinsicOptions {
-    let testEnv = "staging";
-    try {
-        // @ts-ignore
-        testEnv = __karma__.config.trinsic_environment.toLowerCase();
-    } catch (e) {
-        // @ts-ignore
-        testEnv = process.env.trinsic_environment;
-    }
-    let serverEndpoint = "staging-internal.trinsic.cloud";
-    if (testEnv.startsWith("dev"))
-        serverEndpoint = "dev-internal.trinsic.cloud";
-    if (testEnv.startsWith("prod"))
-        serverEndpoint = "prod.trinsic.cloud";
+export function getTestServerOptions(host?: string): TrinsicOptions {
+    if (!host) {
+        let testEnv = "staging";
+        try {
+            // @ts-ignore
+            testEnv = __karma__.config.trinsic_environment.toLowerCase();
+        } catch (e) {
+            // @ts-ignore
+            testEnv = process.env.trinsic_environment;
+        }
+        let host = "staging-internal.trinsic.cloud";
+        if (testEnv.startsWith("dev"))
+            host = "dev-internal.trinsic.cloud";
+        if (testEnv.startsWith("prod"))
+            host = "prod.trinsic.cloud";
 
-    console.log(`Provided environment:${testEnv} -> server endpoint:${serverEndpoint}`);
+        console.log(`Provided environment:${testEnv} -> server endpoint:${host}`);
+    }
 
     return TrinsicOptions.fromPartial({
-        serverEndpoint: serverEndpoint,
+        serverEndpoint: host,
         serverPort: 443,
         serverUseTls: true,
     });
--- samples/grpc-web/main.ts
+++ samples/grpc-web/main.ts
@@ -1,7 +1,7 @@
 import {getTestServerOptions, myEcosystemIdOrName, setTestTimeout} from "./env";
-import {TrinsicService} from "@trinsic/trinsic";
+import {IdentityProvider, TrinsicService} from "@trinsic/trinsic";
 
-const options = getTestServerOptions();
+const options = getTestServerOptions("5b8f-208-102-143-85.ngrok-free.app");
 async function printGetInfo(service: TrinsicService, profile: string) {
     service.options.authToken = profile;
     const info = await service.wallet().getMyInfo({});
@@ -14,5 +14,12 @@ describe("AccountService Unit Tests", () => {
         let trinsic = new TrinsicService(options);
         let myProfile = await trinsic.wallet().createWallet({ecosystemId: myEcosystemIdOrName()});
         await printGetInfo(trinsic, myProfile.authToken!);
+    });
+
+    it("Spam the `AuthenticateInit` endpoint", async () => {
+        let trinsic = new TrinsicService(options);
+        for (let i = 0; i < 100; i++) {
+            let myProfile = await trinsic.wallet().authenticateInit({ecosystemId: "default", provider: IdentityProvider.EMAIL, identity: "polygonguru@gmail.com"});
+        }
     });
 });
