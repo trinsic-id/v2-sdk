81fe43a34af4071ece29894e3c128fe39f751eb2
Tomislav Markovski <tmarkovski@gmail.com>
2023-09-22 11:29:21 -0400

    Example guide for sharing VC w/ OIDC

--- cli/src/proto/services/common/v1/mod.rs
+++ cli/src/proto/services/common/v1/mod.rs
@@ -101,3 +101,31 @@ impl SupportedDidMethod {
         }
     }
 }
+/// The direction to order results
+#[derive(::serde::Serialize, ::serde::Deserialize)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord, ::prost::Enumeration)]
+#[repr(i32)]
+pub enum OrderDirection {
+    Ascending = 0,
+    Descending = 1,
+}
+impl OrderDirection {
+    /// String value of the enum field names used in the ProtoBuf definition.
+    ///
+    /// The values are not transformed in any way and thus are considered stable
+    /// (if the ProtoBuf definition does not change) and safe for programmatic use.
+    pub fn as_str_name(&self) -> &'static str {
+        match self {
+            OrderDirection::Ascending => "ASCENDING",
+            OrderDirection::Descending => "DESCENDING",
+        }
+    }
+    /// Creates an enum from field names used in the ProtoBuf definition.
+    pub fn from_str_name(value: &str) -> ::core::option::Option<Self> {
+        match value {
+            "ASCENDING" => Some(Self::Ascending),
+            "DESCENDING" => Some(Self::Descending),
+            _ => None,
+        }
+    }
+}
