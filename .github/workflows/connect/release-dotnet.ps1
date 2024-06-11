29134d6d6f9b30f9a0261d778a9209034ae4f05c
Scott Phillips <polygonguru@gmail.com>
2024-06-11 16:17:13 -0400

    fix space

--- .github/workflows/connect/release-dotnet.ps1
+++ .github/workflows/connect/release-dotnet.ps1
@@ -16,7 +16,7 @@ if ($ApiKey -eq "")
     Write-Host "No NuGet API key set, skipping push"
     exit 0
 }
-foreach ($item in get-childitem$publishFolder -recurse | select-object FullName | where { $_.FullName.EndsWith(".nupkg") })
+foreach ($item in get-childitem $publishFolder -recurse | select-object FullName | where { $_.FullName.EndsWith(".nupkg") })
 {
     nuget push $item.FullName -ApiKey "$ApiKey" -Source https://api.nuget.org/v3/index.json -SkipDuplicate
 }
