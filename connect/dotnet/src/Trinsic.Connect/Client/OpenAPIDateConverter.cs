74d2678e41b4f7b9f0d7127bd7eb5c7f70723af3
Scott Phillips <polygonguru@gmail.com>
2024-06-12 13:59:10 -0400

    get rid of validation

--- connect/dotnet/src/Trinsic.Connect/Client/OpenAPIDateConverter.cs
+++ connect/dotnet/src/Trinsic.Connect/Client/OpenAPIDateConverter.cs
@@ -9,20 +9,21 @@
 
 using Newtonsoft.Json.Converters;
 
-namespace Trinsic.Connect.Client;
-
-/// <summary>
-/// Formatter for 'date' openapi formats ss defined by full-date - RFC3339
-/// see https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.0.md#data-types
-/// </summary>
-public class OpenAPIDateConverter : IsoDateTimeConverter
+namespace Trinsic.Connect.Client
 {
     /// <summary>
-    /// Initializes a new instance of the <see cref="OpenAPIDateConverter" /> class.
+    /// Formatter for 'date' openapi formats ss defined by full-date - RFC3339
+    /// see https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.0.md#data-types
     /// </summary>
-    public OpenAPIDateConverter()
+    public class OpenAPIDateConverter : IsoDateTimeConverter
     {
-        // full-date   = date-fullyear "-" date-month "-" date-mday
-        DateTimeFormat = "yyyy-MM-dd";
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OpenAPIDateConverter" /> class.
+        /// </summary>
+        public OpenAPIDateConverter()
+        {
+            // full-date   = date-fullyear "-" date-month "-" date-mday
+            DateTimeFormat = "yyyy-MM-dd";
+        }
     }
 }
--- connect/dotnet/src/Trinsic.Connect/Client/WebRequestPathBuilder.cs
+++ connect/dotnet/src/Trinsic.Connect/Client/WebRequestPathBuilder.cs
@@ -10,43 +10,44 @@
 using System;
 using System.Collections.Generic;
 
-namespace Trinsic.Connect.Client;
-
-/// <summary>
-/// A URI builder
-/// </summary>
-internal class WebRequestPathBuilder
+namespace Trinsic.Connect.Client
 {
-    private readonly string _baseUrl;
-    private string _path;
-    private string _query = "?";
-    public WebRequestPathBuilder(string baseUrl, string path)
+    /// <summary>
+    /// A URI builder
+    /// </summary>
+    class WebRequestPathBuilder
     {
-        _baseUrl = baseUrl;
-        _path = path;
-    }
-
-    public void AddPathParameters(Dictionary<string, string> parameters)
-    {
-        foreach (var parameter in parameters)
-        {
-            _path = _path.Replace("{" + parameter.Key + "}", Uri.EscapeDataString(parameter.Value));
-        }
-    }
-
-    public void AddQueryParameters(Multimap<string, string> parameters)
-    {
-        foreach (var parameter in parameters)
-        {
-            foreach (var value in parameter.Value)
+            private string _baseUrl;
+            private string _path;
+            private string _query = "?";
+            public WebRequestPathBuilder(string baseUrl, string path)
             {
-                _query = _query + parameter.Key + "=" + Uri.EscapeDataString(value) + "&";
+                _baseUrl = baseUrl;
+                _path = path;
             }
-        }
-    }
 
-    public string GetFullUri()
-    {
-        return _baseUrl + _path + _query.Substring(0, _query.Length - 1);
+            public void AddPathParameters(Dictionary<string, string> parameters)
+            {
+                foreach (var parameter in parameters)
+                {
+                    _path = _path.Replace("{" + parameter.Key + "}", Uri.EscapeDataString(parameter.Value));
+                }
+            }
+
+            public void AddQueryParameters(Multimap<string, string> parameters)
+            {
+                foreach (var parameter in parameters)
+                {
+                    foreach (var value in parameter.Value)
+                    {
+                        _query = _query + parameter.Key + "=" + Uri.EscapeDataString(value) + "&";
+                    }
+                }
+            }
+
+            public string GetFullUri()
+            {
+                return _baseUrl + _path + _query.Substring(0, _query.Length - 1);
+            }
     }
 }
