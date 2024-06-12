66b6e0579512ed95e711c7f547ecf49ba1568331
Scott Phillips <polygonguru@gmail.com>
2024-06-12 15:30:20 -0400

    fix: Build everything typescript!

--- .github/workflows/connect/build-typescript.ps1
+++ .github/workflows/connect/build-typescript.ps1
@@ -1,3 +1,6 @@
+# Regenerate the swagger too
+Set-Location $PSScriptRoot/../../../
+&./connect/make-swagger.ps1 -language "typescript-fetch" -swaggerFile "connect/swagger_api.json" -outputFolder 'connect/typescript'
 Set-Location $PSScriptRoot/../../../connect/typescript
 # Run the build script
 npm ci
