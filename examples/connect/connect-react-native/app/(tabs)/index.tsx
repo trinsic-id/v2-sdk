c357a2945bb19ec852a2cd1842682576779bf736
Scott Phillips <polygonguru@gmail.com>
2024-06-17 15:13:17 -0400

    fix

--- examples/connect/connect-react-native/app/(tabs)/index.tsx
+++ examples/connect/connect-react-native/app/(tabs)/index.tsx
@@ -1,11 +1,13 @@
-import {Image, StyleSheet, Platform, Button, Linking} from 'react-native';
+import {Image, StyleSheet, Button, Linking} from 'react-native';
 
 import { HelloWave } from '@/components/HelloWave';
+import * as WebBrowser from 'expo-web-browser';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import {CONNECT_RP_AUTH_TOKEN_DO_NOT_COMMIT} from "@/app/(tabs)/SECRET";
-// import {AuthConfiguration, authorize} from "react-native-app-auth";
+import {makeRedirectUri, useAuthRequest} from "expo-auth-session";
+import React from "react";
 
 
 // TODO - Do we want hard-coded dev?
@@ -15,30 +17,30 @@ const redirectUri = `${redirectUriScheme}://mobilecomplete`;
 const url =
     `https://mewmba.ngrok.dev/connect/launch-test?idvProviderSelection=trinsicfake&authToken=${encodeURIComponent(CONNECT_RP_AUTH_TOKEN_DO_NOT_COMMIT)}&redirect_uri=${encodeURIComponent(redirectUri)}`;
 
-// const config: AuthConfiguration = {
-//     clientId: 'N/A', // Not used
-//     clientSecret: 'N/A', // Not used
-//     redirectUrl: redirectUri,
-//     serviceConfiguration: {
-//         authorizationEndpoint: url,
-//         tokenEndpoint: 'N/A',
-//     },
-//     scopes: [] // Not used
-// }
 
-async function openTrinsicConnect(): Promise<void> {
-    console.log('Opening Trinsic Connect');
-    try {
-        // const authState = await authorize(config);
-        const authState = await Linking.openURL(url);
-        console.log('Auth State:', authState);
-    } catch (e) {
-        console.error(e);
-    }
+const config = {
+    redirectUri: makeRedirectUri({scheme: redirectUriScheme, path: "mobilecomplete", native: redirectUri}),
+    clientId: "id.trinsic.connect",
+    scopes: ['N/A'],
+    clientSecret: "N/A"
+};
+const discovery = {
+    authorizationEndpoint: url
+};
 
-}
+WebBrowser.maybeCompleteAuthSession();
 
 export default function HomeScreen() {
+    const [request, response, promptAsync] = useAuthRequest(config,discovery);
+
+    React.useEffect(() => {
+        console.log('Response:', response);
+        if (response?.type === 'success') {
+            const { code } = response.params;
+            console.log('Code:', code);
+        }
+    }, [response]);
+
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
@@ -52,7 +54,12 @@ export default function HomeScreen() {
         <ThemedText type="title">Open Trinsic Connect</ThemedText>
         <HelloWave />
       </ThemedView>
-        <Button title="Connect Wallet" onPress={openTrinsicConnect} />
+        <Button title="Connect Wallet" onPress={() => {
+            console.log('Prompting Login');
+            promptAsync().then((result) => {
+                console.log('Prompt Result:', result);
+            });
+        }} />
     </ParallaxScrollView>
   );
 }
