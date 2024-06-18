fc62199b0afca041d2720630764fad7bf6cb3ccc
Scott Phillips <polygonguru@gmail.com>
2024-06-18 15:27:44 -0400

    build

--- .github/workflows/connect/build-dart.ps1
+++ .github/workflows/connect/build-dart.ps1
@@ -1,3 +1,5 @@
+Set-Location $PSScriptRoot/../../../
+&./connect/make-swagger.ps1 -language "dart" -swaggerFile "connect/swagger_api.json" -outputFolder 'connect/dart'
 Set-Location $PSScriptRoot/../../../connect/dart
 # Run the build script
 dart format .
